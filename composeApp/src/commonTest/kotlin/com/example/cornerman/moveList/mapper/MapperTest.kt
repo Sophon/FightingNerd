9d0866ff9c8b81c6ac93cdb9b256833c8f8354dc
Le Duy Anh <lenduya@gmail.com>
2025-10-28 23:48:48 +0100

    test (movelist): fix imports

--- composeApp/src/commonTest/kotlin/com/example/cornerman/moveList/mapper/MapperTest.kt
+++ composeApp/src/commonTest/kotlin/com/example/cornerman/moveList/mapper/MapperTest.kt
@@ -2,6 +2,17 @@ package com.example.cornerman.moveList.mapper
 
 import assertk.assertThat
 import assertk.assertions.*
+import com.example.cornerman.screens.moveList.mapper.getCategoryName
+import com.example.cornerman.screens.moveList.mapper.getCategorySortOrder
+import com.example.cornerman.screens.moveList.mapper.isBT
+import com.example.cornerman.screens.moveList.mapper.isCD
+import com.example.cornerman.screens.moveList.mapper.isCrouch
+import com.example.cornerman.screens.moveList.mapper.isDirectional
+import com.example.cornerman.screens.moveList.mapper.isMotion
+import com.example.cornerman.screens.moveList.mapper.isNeutralInput
+import com.example.cornerman.screens.moveList.mapper.isStance
+import com.example.cornerman.screens.moveList.mapper.isThrow
+import com.example.cornerman.screens.moveList.mapper.isWS
 import com.example.wikiwavu.domain.model.Move
 import kotlin.test.Test
 
--- composeApp/src/commonTest/kotlin/com/example/cornerman/moveList/util/MoveTest.kt
+++ composeApp/src/commonTest/kotlin/com/example/cornerman/moveList/util/MoveTest.kt
@@ -2,6 +2,7 @@ package com.example.cornerman.moveList.util
 
 import assertk.assertThat
 import assertk.assertions.isEqualTo
+import com.example.cornerman.screens.moveList.util.cleanComboLinks
 import com.example.wikiwavu.domain.model.Move
 import kotlin.test.Test
 
