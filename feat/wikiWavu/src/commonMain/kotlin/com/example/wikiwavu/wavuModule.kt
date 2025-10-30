ffc8177d3b6ebd24c66c9b8ee0118a513616574b
Le Duy Anh <lenduya@gmail.com>
2025-10-30 07:56:05 +0100

    chore (wavu): redundant funs

--- feat/wikiWavu/src/commonMain/kotlin/com/example/wikiwavu/wavuModule.kt
+++ feat/wikiWavu/src/commonMain/kotlin/com/example/wikiwavu/wavuModule.kt
@@ -1,6 +1,5 @@
 package com.example.wikiwavu
 
-import com.example.core.coreModule
 import com.example.wikiwavu.data.WavuWikiDataSource
 import com.example.wikiwavu.data.WavuWikiDataSourceImpl
 import com.example.wikiwavu.domain.WavuUrlProvider
@@ -12,25 +11,13 @@ import com.example.wikiwavu.usecase.FetchMoveDataUseCase
 import com.example.wikiwavu.usecase.FetchMoveListUseCase
 import com.example.wikiwavu.usecase.FetchMovesWithPropertyUseCase
 import com.example.wikiwavu.usecase.GetLastCacheInsertInstantUseCase
-import org.koin.core.context.startKoin
 import org.koin.core.module.dsl.singleOf
-import org.koin.dsl.KoinAppDeclaration
 import org.koin.dsl.bind
 import org.koin.dsl.module
-
-fun initKoin(config: KoinAppDeclaration? = null) = startKoin {
-    config?.invoke(this)
-
-    modules(
-        coreModule,
-        wavuModule,
-    )
-}
 
 val wavuModule = module {
     singleOf(::WavuWikiDataSourceImpl).bind<WavuWikiDataSource>()
     singleOf(::WavuWikiClientImpl).bind<WavuWikiClient>()
-//    singleOf(::InMemoryMoveListDB).bind<com.example.wikiWavu.MoveListDB>()
 
     singleOf(::DownloadCharacterListUseCase)
     singleOf(::DownloadMoveListUseCase)
