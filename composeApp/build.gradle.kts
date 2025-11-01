d6811a1de6bbced853eea43dd19297d8e1df6320
Le Duy Anh <lenduya@gmail.com>
2025-11-01 19:17:28 +0100

    fix (compose): explicitly add nav lib

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -53,6 +53,8 @@ kotlin {
 
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
+            implementation(libs.androidx.navigation.compose)
+            implementation(libs.androidx.navigation.runtime)
 
             api(libs.koin.core)
             implementation(libs.koin.compose)
