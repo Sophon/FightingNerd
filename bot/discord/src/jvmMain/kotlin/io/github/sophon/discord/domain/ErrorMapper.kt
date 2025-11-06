eca61073dec997ce8134f3763718ac5283d0dccd
Le Duy Anh <lenduya@gmail.com>
2025-11-07 00:41:57 +0100

    refactor (bot): remove redundant cases

--- bot/discord/src/jvmMain/kotlin/io/github/sophon/discord/domain/ErrorMapper.kt
+++ bot/discord/src/jvmMain/kotlin/io/github/sophon/discord/domain/ErrorMapper.kt
@@ -8,9 +8,6 @@ internal fun WavuError.toDomain(): BotError {
         WavuError.UNKNOWN_CHARACTER -> BotError.UNKNOWN_CHARACTER
         WavuError.UNKNOWN_MOVE -> BotError.UNKNOWN_MOVE
         WavuError.DOWNLOAD_ERROR -> BotError.DOWNLOAD_ERROR
-        WavuError.CHARACTER_LIST_NOT_FOUND,
-        WavuError.CHARACTER_SERIALIZATION_ERROR,
-            -> BotError.CHARACTER_LIST_FILE_ERROR
         WavuError.DATABASE_ERROR -> BotError.UNKNOWN
     }
 }
--- bot/discord/src/jvmMain/kotlin/io/github/sophon/discord/domain/usecase/GetHeatMovesUseCase.kt
+++ bot/discord/src/jvmMain/kotlin/io/github/sophon/discord/domain/usecase/GetHeatMovesUseCase.kt
@@ -19,9 +19,6 @@ class GetHeatMovesUseCase(
                         WavuError.UNKNOWN_CHARACTER -> BotError.UNKNOWN_CHARACTER
                         WavuError.UNKNOWN_MOVE -> BotError.UNKNOWN_MOVE
                         WavuError.DOWNLOAD_ERROR -> BotError.DOWNLOAD_ERROR
-                        WavuError.CHARACTER_LIST_NOT_FOUND,
-                        WavuError.CHARACTER_SERIALIZATION_ERROR,
-                            -> BotError.CHARACTER_LIST_FILE_ERROR
                         WavuError.DATABASE_ERROR -> BotError.UNKNOWN
                     }
                 )
