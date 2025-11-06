4a7b28c9034eb48bf2d21a26499fea7d9c96b189
Le Duy Anh <lenduya@gmail.com>
2025-11-06 17:54:07 +0100

    refactor (core): tekken 8 properties

--- core/src/commonMain/kotlin/io/github/sophon/core/domain/model/Move.kt
+++ core/src/commonMain/kotlin/io/github/sophon/core/domain/model/Move.kt
@@ -7,31 +7,28 @@ data class Move(
     val charName: String,
     val id: String,
     val input: String,
-    val level: String? = null,
-    val name: String? = null,
-    val parent: String? = null,
     val damage: String? = null,
     val startup: String? = null,
-    val recoveryOnWhiff: String? = null,
-    val totalFrames: String? = null,
-    val crushes: List<String> = listOf(),
     val onBlock: String? = null,
     val onHit: String? = null,
     val onCH: String? = null,
+    val name: String? = null,
+    val recovery: String? = null,
     val notes: List<String> = listOf(),
     val aliases: List <String> = listOf(),
-    val image: String? = null,
     val videoId: String? = null,
-    val alt: String? = null,
 
-    val properties: Properties = Properties(),
+    val t8Properties: T8Properties? = null,
 ) {
-    //null means non applicable
+
     @Serializable
-    data class Properties(
-        val isHeat: Boolean? = null,
-        val isPowerCrush: Boolean? = null,
-        val isHoming: Boolean? = null,
+    data class T8Properties(
+        val level: String? = null,
+        val isHeat: Boolean = false,
+        val isHoming: Boolean = false,
         val stance: String? = null,
+        val isPowerCrush: Boolean = false,
+        val isHighCrush: Boolean = false,
+        val isLowCrush: Boolean = false,
     )
 }
