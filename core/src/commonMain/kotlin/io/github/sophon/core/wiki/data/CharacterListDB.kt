043148388687b4be4b4caa7babe2356164a03b11
Le Duy Anh <lenduya@gmail.com>
2025-11-07 23:24:56 +0100

    feat (core): character list DB

---
+++ core/src/commonMain/kotlin/io/github/sophon/core/wiki/data/CharacterListDB.kt
@@ -0,0 +1,14 @@
+package io.github.sophon.core.wiki.data
+
+import io.github.sophon.core.domain.EmptyResult
+import io.github.sophon.core.domain.Result
+import io.github.sophon.core.wiki.domain.model.Character
+import kotlinx.datetime.Instant
+
+interface CharacterListDB {
+    suspend fun insertCharacterList(characterList: List<Character>): EmptyResult<WikiError>
+    suspend fun fetchCharacterList(): Result<List<Character>, WikiError>
+    suspend fun wipe(): EmptyResult<WikiError>
+    suspend fun getLastInsertTimeStamp(): Result<Instant?, WikiError>
+    suspend fun fetchCharacterDataFor(charName: String): Result<Character, WikiError>
+}
