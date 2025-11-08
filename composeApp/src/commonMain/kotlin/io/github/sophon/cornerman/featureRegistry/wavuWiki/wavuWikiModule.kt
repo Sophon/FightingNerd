441c3d15e6d1276661585d01667bee680aeb75b4
Le Duy Anh <lenduya@gmail.com>
2025-11-08 20:13:36 +0100

    feat (compose): character list caching

--- composeApp/src/commonMain/kotlin/io/github/sophon/cornerman/featureRegistry/wavuWiki/wavuWikiModule.kt
+++ composeApp/src/commonMain/kotlin/io/github/sophon/cornerman/featureRegistry/wavuWiki/wavuWikiModule.kt
@@ -2,7 +2,7 @@ package io.github.sophon.cornerman.featureRegistry.wavuWiki
 
 import io.github.sophon.cornerman.featureRegistry.wavuWiki.ui.WavuHomeScreenVM
 import io.github.sophon.cornerman.featureRegistry.wavuWiki.usecase.FetchCharacterListUseCase
-import io.github.sophon.cornerman.featureRegistry.wavuWiki.usecase.StartWavuSessionUseCase
+import io.github.sophon.cornerman.featureRegistry.wavuWiki.usecase.SyncDataIfOldUseCase
 import io.github.sophon.wikiwavu.infrastructure.FileReader
 import org.koin.core.module.dsl.singleOf
 import org.koin.core.module.dsl.viewModelOf
@@ -12,7 +12,7 @@ import org.koin.dsl.module
 internal val wavuWikiFeatureModule = module {
     viewModelOf(::WavuHomeScreenVM)
 
-    singleOf(::StartWavuSessionUseCase)
+    singleOf(::SyncDataIfOldUseCase)
     singleOf(::FetchCharacterListUseCase)
 
     singleOf(::FileReaderKMP).bind<FileReader>()
