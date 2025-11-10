690d60b7fced58ddc31cdd5eb934605d343d1635
Le Duy Anh <lenduya@gmail.com>
2025-11-10 20:42:42 +0100

    refactor (bot): extract query parsing to a file

--- bot/discord/src/jvmMain/kotlin/io/github/sophon/discord/featureRegistry/wikiWavu/usecase/SearchFrameDataUseCase.kt
+++ bot/discord/src/jvmMain/kotlin/io/github/sophon/discord/featureRegistry/wikiWavu/usecase/SearchFrameDataUseCase.kt
@@ -1,17 +1,17 @@
 package io.github.sophon.discord.featureRegistry.wikiWavu.usecase
 
 import io.github.sophon.core.domain.Result
-import io.github.sophon.core.util.dropFirstAndJoin
 import io.github.sophon.core.wiki.domain.model.Move
 import io.github.sophon.discord.BotError
 import io.github.sophon.discord.domain.toDomainError
+import io.github.sophon.discord.util.parseQuery
 import io.github.sophon.wikiwavu.WavuWikiClient
 
 internal class SearchFrameDataUseCase(
     private val wavuWikiClient: WavuWikiClient,
 ) {
     suspend fun invoke(query: String): Result<Move, BotError> {
-        val parsedQuery = parseQuery(query)
+        val parsedQuery = query.parseQuery()
         if (parsedQuery == null) return Result.Error(BotError.INVALID_QUERY)
 
         return when (
@@ -21,18 +21,4 @@ internal class SearchFrameDataUseCase(
             is Result.Error -> Result.Error(result.error.toDomainError())
         }
     }
-
-    private fun parseQuery(query: String): ParsedQuery? {
-        if (query.split(" ").size < 2) return null
-
-        val charName = query.substringBefore(' ')
-        val move = query.dropFirstAndJoin(' ')
-
-        return ParsedQuery(charName, move)
-    }
-
-    private data class ParsedQuery(
-        val charName: String,
-        val move: String,
-    )
 }
