f0b8a99173b9d2d57b9158d297e08af7d94d2bf7
Le Duy Anh <lenduya@gmail.com>
2025-11-16 21:17:09 +0100

    test (core): exclude from test coverage annotation

--- feat/glossaryInfil/src/commonMain/kotlin/io/github/sophon/glossaryinfil/usecase/GetFeatureInfoUseCase.kt
+++ feat/glossaryInfil/src/commonMain/kotlin/io/github/sophon/glossaryinfil/usecase/GetFeatureInfoUseCase.kt
@@ -1,11 +1,13 @@
 package io.github.sophon.glossaryinfil.usecase
 
+import io.github.sophon.core.domain.ExcludeFromCoverage
 import io.github.sophon.core.feature.FeatureInfo
 import io.github.sophon.glossaryinfil.BuildKonfig
 import io.github.sophon.glossaryinfil.FEATURE_IMG_URL
 import io.github.sophon.glossaryinfil.FEATURE_NAME
 import io.github.sophon.glossaryinfil.FEATURE_URL
 
+@ExcludeFromCoverage("Feature info static set")
 internal class GetFeatureInfoUseCase {
     fun invoke(): FeatureInfo {
         return FeatureInfo(
