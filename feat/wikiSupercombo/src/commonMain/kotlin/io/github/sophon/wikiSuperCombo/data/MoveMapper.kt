a8dca7ef7eb4b398effb900789923ace7687ba65
Le Duy Anh <lenduya@gmail.com>
2025-11-22 00:56:10 +0100

    refactor (supercombo): reorganize Move properties

--- feat/wikiSupercombo/src/commonMain/kotlin/io/github/sophon/wikiSuperCombo/data/MoveMapper.kt
+++ feat/wikiSupercombo/src/commonMain/kotlin/io/github/sophon/wikiSuperCombo/data/MoveMapper.kt
@@ -12,35 +12,39 @@ internal fun MoveListResponseDto.toDomain(
         Move(
             charName = dto.chara,
             id = dto.moveId,
+            name = dto.name,
+
             input = dto.input.lowercase(),
             damage = dto.damage.takeIfNotTemplate()?.cleanHtml(),
             startup = dto.startup.takeIfNotTemplate(),
             onBlock = dto.blockAdv.takeIfNotTemplate()?.cleanHtml(),
             onHit = dto.hitAdv.takeIfNotTemplate()?.cleanHtml(),  
             onCH = null,
-            name = dto.name,
             recovery = dto.recovery.takeIfNotTemplate()?.cleanHtml(),
             notes = dto.notes.takeIfNotTemplate()?.cleanHtml()
                 .extractNotes(),
             active = dto.active.takeIfNotTemplate()?.cleanHtml(),
+            guard = dto.guard.takeIfNotTemplate(),
+            cancel = dto.cancel.takeIfNotTemplate(),
+            invulnerability = dto.invuln.takeIfNotTemplate()?.cleanHtml(),
 
             urls = Move.Urls(
-                hitboxImage = dto.hitboxes?.let { imageUrlMap[it] },
+                hitboxImage = dto.hitboxes
+                    ?.takeIfNotTemplate()
+                    ?.split(",")
+                    .orEmpty()
+                    .firstOrNull()
+                    ?.let { imageUrlMap[it] },
             ),
 
             sf6Properties = Move.SF6Properties(
                 type = dto.moveType,
-                guard = dto.guard.takeIfNotTemplate(),
                 images = dto.images.takeIfNotTemplate()
-                    ?.split(",")
-                    ?.map { it.trim() },
-                hitboxes = dto.hitboxes.takeIfNotTemplate()
                     ?.split(",")
                     ?.map { it.trim() },
                 chip = dto.chip.takeIfNotTemplate(),
                 dmgScaling = dto.dmgScaling.takeIfNotTemplate(),
                 total = dto.total.takeIfNotTemplate(),
-                cancel = dto.cancel.takeIfNotTemplate(),
                 hitConfirm = dto.hitconfirm.takeIfNotTemplate(),
                 punishAdv = dto.punishAdv.takeIfNotTemplate()?.cleanHtml(),  
                 perfParryAdv = dto.perfParryAdv.takeIfNotTemplate()?.cleanHtml(),  
@@ -56,7 +60,6 @@ internal fun MoveListResponseDto.toDomain(
                 driveGain = dto.driveGain.takeIfNotTemplate(),
                 superGainOnHit = dto.superGainHit.takeIfNotTemplate(),
                 superGainOnBlock = dto.superGainBlk.takeIfNotTemplate(),
-                invulnerability = dto.invuln.takeIfNotTemplate()?.cleanHtml(),  
                 armor = dto.armor.takeIfNotTemplate(),
                 jugStart = dto.jugStart.takeIfNotTemplate()?.cleanHtml(),  
                 jugIncrease = dto.jugIncrease.takeIfNotTemplate()?.cleanHtml(),  
