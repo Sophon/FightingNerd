86f82404abc891c85835e8ba97bb0f97df89c28f
Le Duy Anh <lenduya@gmail.com>
2025-11-22 05:47:06 +0100

    refactor (dreamcancel): concurrent move downloads

--- feat/wikiDreamCancel/src/commonMain/kotlin/io/github/sophon/dreamcancel/DreamCancelConstants.kt
+++ feat/wikiDreamCancel/src/commonMain/kotlin/io/github/sophon/dreamcancel/DreamCancelConstants.kt
@@ -4,5 +4,3 @@ internal const val FEATURE_NAME = "DreamCancel Wiki"
 internal const val FEATURE_URL = "https://dreamcancel.com/wiki"
 internal const val FEATURE_IMG_URL = "https://i.imgur.com/iGye4Fy.png"
 internal const val BASE_URL = "https://dreamcancel.com/w/api.php"
-
-internal const val LIMIT_MOVES = 500
--- feat/wikiDreamCancel/src/commonMain/kotlin/io/github/sophon/dreamcancel/data/DreamCancelWikiDataSource.kt
+++ feat/wikiDreamCancel/src/commonMain/kotlin/io/github/sophon/dreamcancel/data/DreamCancelWikiDataSource.kt
@@ -8,54 +8,63 @@ import io.github.sophon.dreamcancel.BASE_URL
 import io.ktor.client.HttpClient
 import io.ktor.client.request.get
 import io.ktor.client.request.parameter
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flatMapMerge
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.toList
 
 interface DreamCancelWikiDataSource {
     suspend fun downloadData(table: String): Result<MoveListResponseDto, DataError.Remote>
     suspend fun getImageUrl(fileNames: List<String>): Result<Map<String, String>, DataError.Remote>
 }
 
+@OptIn(ExperimentalCoroutinesApi::class)
 internal class DreamCancelWikiDataSourceImpl(
     private val httpClient: HttpClient
 ): DreamCancelWikiDataSource {
-
     override suspend fun downloadData(
         table: String,
     ): Result<MoveListResponseDto, DataError.Remote> {
-        val allTitles = mutableListOf<Title>()
-        var offset = 0
-        val limit = 500
-        val maxPages = 10
-        var pageCount = 0
-
-        do {
-            if (pageCount >= maxPages) break
-
-            val result = safeCall<MoveListResponseDto> {
-                httpClient.get(BASE_URL) {
-                    parameter("action", "cargoquery")
-                    parameter("tables", table)
-                    parameter("fields", getDataFields())
-                    parameter("format", "json")
-                    parameter("limit", limit)
-                    parameter("offset", offset)
+        //list of <offset;Result>
+        val results = flow {
+            repeat(NO_MAX_PAGES) { page ->
+                emit(page * NO_MAX_MOVES)
+            }
+        }
+            .flatMapMerge(concurrency = NO_MAX_CONCURRENT) { offset ->
+                flow {
+                    val result = safeCall<MoveListResponseDto> {
+                        httpClient.get(BASE_URL) {
+                            parameter("action", "cargoquery")
+                            parameter("tables", table)
+                            parameter("fields", getDataFields())
+                            parameter("format", "json")
+                            parameter("limit", NO_MAX_MOVES)
+                            parameter("offset", offset)
+                        }
+                    }
+                    emit(offset to result)
                 }
             }
+            .toList()
 
-            when (result) {
-                is Result.Success -> {
-                    val moves = result.data.cargoQuery
-                    allTitles.addAll(moves)
+        val firstError = results.firstOrNull { it.second is Result.Error }
+        if (firstError != null) {
+            return firstError.second as Result.Error
+        }
 
-                    if (moves.size < limit) break
+        val allCargoQueries = mutableListOf<Title>()
+        val resultsSortedByOffset = results.sortedBy { it.first }
 
-                    offset += limit
-                    pageCount++
-                }
-                is Result.Error -> return result
-            }
-        } while (true)
+        for ((_, result) in resultsSortedByOffset) {
+            val cargoQueries = (result as Result.Success).data.cargoQuery
+            allCargoQueries.addAll(cargoQueries)
 
-        return Result.Success(MoveListResponseDto(cargoQuery = allTitles))
+            //page with less data than max per page -> finished
+            if (cargoQueries.size < NO_MAX_MOVES) break
+        }
+
+        return Result.Success(MoveListResponseDto(cargoQuery = allCargoQueries))
     }
 
     override suspend fun getImageUrl(
@@ -66,6 +75,13 @@ internal class DreamCancelWikiDataSourceImpl(
             fileNames = fileNames,
             url = BASE_URL,
         )
+    }
+
+
+    private companion object {
+        const val NO_MAX_PAGES = 10
+        const val NO_MAX_MOVES = 500
+        const val NO_MAX_CONCURRENT = 5
     }
 }
 
