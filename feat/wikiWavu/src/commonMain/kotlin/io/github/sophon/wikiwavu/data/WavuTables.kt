118ff27439640447d367e1c959c24012881be28d
Le Duy Anh <lenduya@gmail.com>
2025-11-23 22:07:05 +0100

    refactor (wavu): use core Game

--- feat/wikiWavu/src/commonMain/kotlin/io/github/sophon/wikiwavu/data/WavuTables.kt
+++ feat/wikiWavu/src/commonMain/kotlin/io/github/sophon/wikiwavu/data/WavuTables.kt
@@ -1,18 +1,17 @@
 package io.github.sophon.wikiwavu.data
 
+import io.github.sophon.core.feature.Game
 import io.github.sophon.core.wiki.data.QueryTable
 
 internal object WavuTables {
     private val gameTables = mapOf(
-        "Tekken_8" to QueryTable(
+        Game.Tekken8.id to QueryTable(
             character = "not used",
             moves = TABLE_T8_MOVE_LIST,
         ),
     )
 
     fun getTable(gameId: String): QueryTable? = gameTables[gameId]
-
-    fun supportedGames(): Set<String> = gameTables.keys
 }
 
 private const val TABLE_T8_MOVE_LIST = "Move"
--- feat/wikiWavu/src/commonMain/kotlin/io/github/sophon/wikiwavu/domain/WavuFeatureInfo.kt
+++ feat/wikiWavu/src/commonMain/kotlin/io/github/sophon/wikiwavu/domain/WavuFeatureInfo.kt
@@ -1,6 +1,7 @@
 package io.github.sophon.wikiwavu.domain
 
 import io.github.sophon.core.feature.FeatureInfo
+import io.github.sophon.core.feature.Game
 import io.github.sophon.wikiwavu.BuildKonfig
 import io.github.sophon.wikiwavu.FEATURE_IMG_URL
 import io.github.sophon.wikiwavu.FEATURE_NAME
@@ -12,7 +13,9 @@ object WavuFeatureInfo {
         name = FEATURE_NAME,
         url = FEATURE_URL,
         iconUrl = FEATURE_IMG_URL,
-        supportedGames = WavuTables.supportedGames(),
+        supportedGameSet = setOf(
+            Game.Tekken8,
+        ),
         version = BuildKonfig.VERSION,
     )
 }
