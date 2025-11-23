697b5cc9202792f0140596b512d2c8107c61b161
Le Duy Anh <lenduya@gmail.com>
2025-11-23 01:18:14 +0100

    test (dreamcancel): simplify tests

--- feat/wikiDreamCancel/src/commonTest/kotlin/io/github/sophon/dreamcancel/data/CharacterMapperTest.kt
+++ feat/wikiDreamCancel/src/commonTest/kotlin/io/github/sophon/dreamcancel/data/CharacterMapperTest.kt
@@ -2,165 +2,119 @@ package io.github.sophon.dreamcancel.data
 
 import assertk.assertThat
 import assertk.assertions.isEqualTo
-import io.github.sophon.core.wiki.domain.model.Character
 import kotlin.test.Test
 
 class CharacterMapperTest {
+    //region Alias handling
     @Test
-    fun `toDomain handles names with spaces`() {
+    fun `createAliases handles names with spaces`() {
+        //given
         val characterName = "B. Jenet"
-        val expectedCharacter = Character(
-            id = "b_jenet",
-            displayName = "B. Jenet",
-            queryName = "B._Jenet",
-            wikiUrl = "https://dreamcancel.com/wiki/Fatal_Fury:_City_of_the_Wolves/B._Jenet",
-            aliasList = listOf("jenet", "bj")
+        val expected = listOf(
+            "jenet",
+            "bj",
         )
 
-        // when
-        val result = characterName.toDomain(TAG_COTW)
+        //when
+        val result = characterName.createAliases()
 
-        // then
-        assertThat(result).isEqualTo(expectedCharacter)
+        //then
+        assertThat(result).isEqualTo(expected)
     }
 
-    //region Alias Generation Tests
     @Test
-    fun `toDomain should create empty aliases for single word name`() {
-        // given
+    fun `createAliases handles single word names`() {
+        //given
         val characterName = "King"
-        val expectedCharacter = Character(
-            id = "king",
-            displayName = "King",
-            queryName = "King",
-            wikiUrl = "https://dreamcancel.com/wiki/The_King_of_Fighters_XV/King",
-            aliasList = emptyList()
-        )
+        val expected = listOf<String>()
 
-        // when
-        val result = characterName.toDomain(TAG_KOF)
+        //when
+        val result = characterName.createAliases()
 
-        // then
-        assertThat(result).isEqualTo(expectedCharacter)
+        //then
+        assertThat(result).isEqualTo(expected)
     }
 
     @Test
-    fun `toDomain should create aliases for two word name`() {
+    fun `createAliases handles two word names`() {
         // given
         val characterName = "Andy Bogard"
-        val expectedCharacter = Character(
-            id = "andy_bogard",
-            displayName = "Andy Bogard",
-            queryName = "Andy_Bogard",
-            wikiUrl = "https://dreamcancel.com/wiki/The_King_of_Fighters_XV/Andy_Bogard",
-            aliasList = listOf("andy", "bogard", "ab")
+        val expected = listOf(
+            "andy",
+            "bogard",
+            "ab",
         )
 
-        // when
-        val result = characterName.toDomain(TAG_KOF)
+        //when
+        val result = characterName.createAliases()
 
-        // then
-        assertThat(result).isEqualTo(expectedCharacter)
+        //then
+        assertThat(result).isEqualTo(expected)
     }
 
     @Test
-    fun `toDomain should create aliases for three word name with short word`() {
-        // given
+    fun `createAliases handles three word names`() {
+        //given
         val characterName = "King of Dinosaurs"
-        val expectedCharacter = Character(
-            id = "king_of_dinosaurs",
-            displayName = "King of Dinosaurs",
-            queryName = "King_of_Dinosaurs",
-            wikiUrl = "https://dreamcancel.com/wiki/The_King_of_Fighters_XV/King_of_Dinosaurs",
-            aliasList = listOf("king", "dinosaurs", "of", "kod")
+        val expected = listOf(
+            "king",
+            "dinosaurs",
+            "kod",
         )
 
-        // when
-        val result = characterName.toDomain(TAG_KOF)
+        //when
+        val result = characterName.createAliases()
 
-        // then
-        assertThat(result).isEqualTo(expectedCharacter)
+        //then
+        assertThat(result).isEqualTo(expected)
     }
 
     @Test
-    fun `toDomain should handle duplicate words in name`() {
+    fun `createAliases handles duplicate words`() {
         // given
         val characterName = "Sylvie Paula Paula"
-        val expectedCharacter = Character(
-            id = "sylvie_paula_paula",
-            displayName = "Sylvie Paula Paula",
-            queryName = "Sylvie_Paula_Paula",
-            wikiUrl = "https://dreamcancel.com/wiki/The_King_of_Fighters_XV/Sylvie_Paula_Paula",
-            aliasList = listOf("sylvie", "paula", "spp")
+        val expected = listOf(
+            "sylvie",
+            "paula",
+            "spp",
         )
 
-        // when
-        val result = characterName.toDomain(TAG_KOF)
+        //when
+        val result = characterName.createAliases()
 
-        // then
-        assertThat(result).isEqualTo(expectedCharacter)
+        //then
+        assertThat(result).isEqualTo(expected)
     }
 
     @Test
-    fun `toDomain should filter out single character words from aliases`() {
-        // given
-        val characterName = "K&#039;"
-        val expectedCharacter = Character(
-            id = "k",
-            displayName = "K'",
-            queryName = "K'",
-            wikiUrl = "https://dreamcancel.com/wiki/The_King_of_Fighters_XV/K'",
-            aliasList = listOf()
+    fun `createAliases handles three words`() {
+        //given
+        val characterName = "King of Dinosaurs"
+        val expected = listOf(
+            "king",
+            "dinosaurs",
+            "kod",
         )
 
-        // when
-        val result = characterName.toDomain(TAG_KOF)
+        //when
+        val result = characterName.createAliases()
 
-        // then
-        assertThat(result).isEqualTo(expectedCharacter)
-    }
-
-    @Test
-    fun `toDomain should handle accented characters`() {
-        // given
-        val characterName = "Ángel"
-        val expectedCharacter = Character(
-            id = "angel",
-            displayName = "Ángel",
-            queryName = "Angel",
-            wikiUrl = "https://dreamcancel.com/wiki/The_King_of_Fighters_XV/Angel",
-            aliasList = emptyList()
-        )
-
-        // when
-        val result = characterName.toDomain(TAG_KOF)
-
-        // then
-        assertThat(result).isEqualTo(expectedCharacter)
-    }
-
-    @Test
-    fun `toDomain should handle period in name`() {
-        // given
-        val characterName = "B.Jenet"
-        val expectedCharacter = Character(
-            id = "b_jenet",
-            displayName = "B.Jenet",
-            queryName = "B.Jenet",
-            wikiUrl = "https://dreamcancel.com/wiki/The_King_of_Fighters_XV/B.Jenet",
-            aliasList = listOf("jenet", "bj")
-        )
-
-        // when
-        val result = characterName.toDomain(TAG_KOF)
-
-        // then
-        assertThat(result).isEqualTo(expectedCharacter)
+        //then
+        assertThat(result).isEqualTo(expected)
     }
     //endregion
 
-    private companion object {
-        const val TAG_KOF = "The_King_of_Fighters_XV"
-        const val TAG_COTW = "Fatal_Fury:_City_of_the_Wolves"
+    //region query name
+    @Test
+    fun `createQueryName handles names with spaces`() {
+        val characterName = "B. Jenet"
+        val expected = "B._Jenet"
+
+        //when
+        val result = characterName.createQueryName()
+
+        //then
+        assertThat(result).isEqualTo(expected)
     }
+    //endregion
 }
