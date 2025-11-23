137c5a08dfd89ccec54b08d0d75632b4c61c618a
Le Duy Anh <lenduya@gmail.com>
2025-11-23 22:06:58 +0100

    refactor (supercombo): use core Game

--- feat/wikiSupercombo/src/commonMain/kotlin/io/github/sophon/wikiSuperCombo/domain/SuperComboFeatureInfo.kt
+++ feat/wikiSupercombo/src/commonMain/kotlin/io/github/sophon/wikiSuperCombo/domain/SuperComboFeatureInfo.kt
@@ -1,18 +1,20 @@
 package io.github.sophon.wikiSuperCombo.domain
 
 import io.github.sophon.core.feature.FeatureInfo
+import io.github.sophon.core.feature.Game
 import io.github.sophon.wikiSuperCombo.BuildKonfig
 import io.github.sophon.wikiSuperCombo.FEATURE_IMG_URL
 import io.github.sophon.wikiSuperCombo.FEATURE_NAME
 import io.github.sophon.wikiSuperCombo.FEATURE_URL
-import io.github.sophon.wikiSuperCombo.data.SuperComboTables
 
 object SuperComboFeatureInfo {
     val featureInfo = FeatureInfo(
         name = FEATURE_NAME,
         url = FEATURE_URL,
         iconUrl = FEATURE_IMG_URL,
-        supportedGames = SuperComboTables.supportedGames(),
+        supportedGameSet = setOf(
+            Game.StreetFighter6,
+        ),
         version = BuildKonfig.VERSION,
     )
 }
