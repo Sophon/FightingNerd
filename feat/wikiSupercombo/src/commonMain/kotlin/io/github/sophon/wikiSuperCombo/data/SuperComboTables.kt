df0ec08f92a3651439c657813c9f24c7257fd23c
Le Duy Anh <lenduya@gmail.com>
2025-12-10 12:35:00 +0100

    feat (superCombo): tables

--- feat/wikiSupercombo/src/commonMain/kotlin/io/github/sophon/wikiSuperCombo/data/SuperComboTables.kt
+++ feat/wikiSupercombo/src/commonMain/kotlin/io/github/sophon/wikiSuperCombo/data/SuperComboTables.kt
@@ -7,8 +7,12 @@ internal object SuperComboTables {
     private val gameToTables = mapOf(
         Game.StreetFighter6.id to QueryTable(
             character = TABLE_SF6_CHARACTERS,
-            moves = TABLE_SF6_MOVE_LIST
+            moves = TABLE_SF6_MOVE_LIST,
         ),
+        Game.MK1.id to QueryTable(
+            character = TABLE_MK1_CHARACTERS,
+            moves = TABLE_MK1_MOVE_LIST,
+        )
     )
 
     fun getTable(gameId: String): QueryTable? {
@@ -18,3 +22,5 @@ internal object SuperComboTables {
 
 private const val TABLE_SF6_CHARACTERS = "SF6_CharacterData"
 private const val TABLE_SF6_MOVE_LIST = "SF6_FrameData"
+private const val TABLE_MK1_CHARACTERS = "MK1_CharacterData"
+private const val TABLE_MK1_MOVE_LIST = "MK1_FrameData"
