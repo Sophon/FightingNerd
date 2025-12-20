42a460b8e3d8f84c069f5f0236daa4583560ee93
Le Duy Anh <lenduya@gmail.com>
2025-12-20 11:14:40 +0100

    feat (admin): DI repo

--- feat/admin/src/commonMain/kotlin/io/github/sophon/adminModule.kt
+++ feat/admin/src/commonMain/kotlin/io/github/sophon/adminModule.kt
@@ -1,5 +1,7 @@
 package io.github.sophon
 
+import io.github.sophon.data.BanRepo
+import io.github.sophon.data.BanRepoImpl
 import io.github.sophon.domain.AdminFeatureInfo
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
@@ -8,4 +10,6 @@ import org.koin.dsl.module
 fun adminModule() = module {
     single { AdminFeatureInfo }
     singleOf(::AdminToolImpl).bind<AdminTool>()
+
+    singleOf(::BanRepoImpl).bind<BanRepo>()
 }
