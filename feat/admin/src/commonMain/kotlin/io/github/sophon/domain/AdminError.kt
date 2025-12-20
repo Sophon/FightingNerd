a0f54779ec438e3bd2494eaaa4c7757216f7bd88
Le Duy Anh <lenduya@gmail.com>
2025-12-20 21:23:27 +0100

    test (admin): usecase test

--- feat/admin/src/commonMain/kotlin/io/github/sophon/domain/AdminError.kt
+++ feat/admin/src/commonMain/kotlin/io/github/sophon/domain/AdminError.kt
@@ -5,7 +5,7 @@ import io.github.sophon.core.domain.Error
 sealed class AdminError(vararg val errors: String): Error {
     class DatabaseError(error: String): AdminError(error)
     class PermissionDenied(): AdminError("Permission denied")
-    class WrongReplyFormat(query: String): AdminError(query)
+    class WrongReplyFormat(val query: String): AdminError(query)
 
     override fun toString(): String =
         "${this::class.simpleName}(${errors.joinToString()})"
---
+++ feat/admin/src/jvmTest/kotlin/io/github/sophon/domain/usecase/CreateReplyUseCaseTest.kt
@@ -0,0 +1,77 @@
+package io.github.sophon.domain.usecase
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import assertk.assertions.isInstanceOf
+import io.github.sophon.core.domain.Result
+import io.github.sophon.domain.AdminError
+import io.github.sophon.domain.AdminResult
+import io.github.sophon.domain.Source
+import kotlin.test.Test
+
+class CreateReplyUseCaseTest {
+    private val usecase = CreateReplyUseCase()
+
+    @Test
+    fun `usecase handles normal input`() {
+        //given
+        val string = "phd_cunnilingus-126041495038853120-717398042562658347 some feedback"
+        val expected = Result.Success(
+            AdminResult(
+                Source(
+                    username = "phd_cunnilingus",
+                    id = "126041495038853120",
+                    channelId = "717398042562658347",
+                ),
+                message = "some feedback",
+            )
+        )
+
+        //when
+        val result = usecase.invoke(string)
+
+        //then
+        assertThat(result).isEqualTo(expected)
+    }
+
+    @Test
+    fun `usecase handles missing source`() {
+        //given
+        val string = "phd_cunnilingus-126041495038853120- some feedback"
+
+        //when
+        val result = usecase.invoke(string)
+        val error = (result as Result.Error).error
+
+        //then
+        assertThat(result).isInstanceOf(Result.Error::class.java)
+
+        assertThat(error).isInstanceOf(AdminError.WrongReplyFormat::class.java)
+        val wrongFormat = error as AdminError.WrongReplyFormat
+
+        assertThat(wrongFormat.query).isEqualTo(string)
+    }
+
+    @Test
+    fun `usecase handles missing message`() {
+        //given
+        val string = "phd_cunnilingus-126041495038853120-717398042562658347"
+        val expected = Result.Error(
+            AdminError.WrongReplyFormat(
+                "phd_cunnilingus-126041495038853120-717398042562658347"
+            )
+        )
+
+        //when
+        val result = usecase.invoke(string)
+        val error = (result as Result.Error).error
+
+        //then
+        assertThat(result).isInstanceOf(Result.Error::class.java)
+
+        assertThat(error).isInstanceOf(AdminError.WrongReplyFormat::class.java)
+        val wrongFormat = error as AdminError.WrongReplyFormat
+
+        assertThat(wrongFormat.query).isEqualTo(string)
+    }
+}
