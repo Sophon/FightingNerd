e9960898934ef5c94bd730eb76f6fbfd96395ccd
Le Duy Anh <lenduya@gmail.com>
2026-01-04 00:56:03 +0100

    feat (mizuumi): finish mizuumi wiki client

--- feat/wikiMizuumi/src/commonMain/kotlin/io/github/sophon/wikimizuumi/MizuumiWikiClient.kt
+++ feat/wikiMizuumi/src/commonMain/kotlin/io/github/sophon/wikimizuumi/MizuumiWikiClient.kt
@@ -1,8 +1,13 @@
 package io.github.sophon.wikimizuumi
 
+import io.github.aakira.napier.Napier
 import io.github.sophon.core.domain.EmptyResult
 import io.github.sophon.core.domain.Result
+import io.github.sophon.core.domain.map
+import io.github.sophon.core.domain.onError
+import io.github.sophon.core.domain.onSuccess
 import io.github.sophon.core.feature.FeatureInfo
+import io.github.sophon.core.wiki.data.QueryTable
 import io.github.sophon.core.wiki.data.WikiError
 import io.github.sophon.core.wiki.domain.WikiClient
 import io.github.sophon.core.wiki.domain.model.Character
@@ -10,13 +15,14 @@ import io.github.sophon.core.wiki.domain.model.Move
 import io.github.sophon.core.wiki.usecase.CacheCharacterListUseCase
 import io.github.sophon.core.wiki.usecase.CacheMoveListUseCase
 import io.github.sophon.core.wiki.usecase.ClearCacheUseCase
-import io.github.sophon.core.wiki.usecase.DownloadCharacterListUseCase
 import io.github.sophon.core.wiki.usecase.DownloadMoveListUseCase
+import io.github.sophon.core.wiki.usecase.DownloadOrFetchUseCase
 import io.github.sophon.core.wiki.usecase.FetchCharacterListUseCase
 import io.github.sophon.core.wiki.usecase.FetchCharacterUseCase
 import io.github.sophon.core.wiki.usecase.FetchMoveListUseCase
 import io.github.sophon.core.wiki.usecase.FetchMoveUseCase
 import io.github.sophon.core.wiki.usecase.GetLastCacheInsertInstantUseCase
+import io.github.sophon.wikimizuumi.data.MizuumiTables
 import kotlinx.datetime.Instant
 import kotlin.time.ExperimentalTime
 
@@ -24,14 +30,14 @@ import kotlin.time.ExperimentalTime
 internal class MizuumiWikiClient(
     private val gameId: String,
 
-    //feature info
+    private val mizuumiFeatureInfo: MizuumiFeatureInfo,
 
-    private val downloadCharacterListUseCase: DownloadCharacterListUseCase,
+    private val downloadOrFetchUseCase: DownloadOrFetchUseCase,
+
     private val cacheCharacterListUseCase: CacheCharacterListUseCase,
     private val fetchCharacterUseCase: FetchCharacterUseCase,
     private val fetchCharacterListUseCase: FetchCharacterListUseCase,
 
-    private val downloadMoveListUseCase: DownloadMoveListUseCase,
     private val cacheMoveListUseCase: CacheMoveListUseCase,
     private val clearCacheUseCase: ClearCacheUseCase,
     private val fetchMoveListUseCase: FetchMoveListUseCase,
@@ -39,53 +45,87 @@ internal class MizuumiWikiClient(
     private val getLastCacheInsertInstantUseCase: GetLastCacheInsertInstantUseCase,
     private val fetchMoveUseCase: FetchMoveUseCase,
 ): WikiClient {
-    override fun getFeatureInfo(): FeatureInfo {
-        TODO("Not yet implemented")
-    }
+    private val gameTables: QueryTable = MizuumiTables.getTable(gameId)
+        ?: error("$gameId not supported. Supported: ${MizuumiFeatureInfo.featureInfo.supportedGameSet}")
+
+    override fun getFeatureInfo(): FeatureInfo = mizuumiFeatureInfo.featureInfo
 
     override suspend fun downloadCharacterList(): Result<List<Character>, WikiError> {
-        TODO("Not yet implemented")
+        return downloadOrFetchUseCase.invoke(gameTables)
+            .map { it.keys.toList() }
+            .onSuccess { characterList ->
+                Napier.i(tag = TAG) { "$gameId - ${characterList.size} characters downloaded" }
+            }
+            .onError { Napier.e(tag = TAG) { "downloadCharacterList: $it" } }
     }
 
-    override suspend fun cacheCharacterList(characterList: List<Character>): EmptyResult<WikiError> {
-        TODO("Not yet implemented")
+    override suspend fun cacheCharacterList(
+        characterList: List<Character>
+    ): EmptyResult<WikiError> {
+        return cacheCharacterListUseCase.invoke(characterList)
+            .onError { Napier.e(tag = TAG) { "cacheCharacterList: $it" } }
     }
 
     override suspend fun fetchCharacterList(): Result<List<Character>, WikiError> {
-        TODO("Not yet implemented")
+        return fetchCharacterListUseCase.invoke()
+            .onError { Napier.e(tag = TAG) { "fetchCharacterList: $it" } }
     }
 
     override suspend fun fetchCharacter(charName: String): Result<Character, WikiError> {
-        TODO("Not yet implemented")
+        return fetchCharacterUseCase.invoke(charName)
+            .onError { Napier.w(tag = TAG) { "fetchCharacter: $it" } }
     }
 
-    override suspend fun downloadMoveList(characterData: DownloadMoveListUseCase.CharacterData): Result<List<Move>, WikiError> {
-        TODO("Not yet implemented")
+    override suspend fun downloadMoveList(
+        characterData: DownloadMoveListUseCase.CharacterData,
+    ): Result<List<Move>, WikiError> {
+        return downloadOrFetchUseCase.invoke(gameTables)
+            .map { map ->
+                map
+                    .filterKeys { it.queryName.equals(characterData.name, ignoreCase = true) }
+                    .values
+                    .flatten()
+            }
+            .onSuccess { moveList ->
+                Napier.d(tag = TAG) { "${characterData.name}: ${moveList.size} moves downloaded" }
+            }
+            .onError { Napier.e(tag = TAG) { "downloadMoveList: $it" } }
     }
 
     override suspend fun cacheMoveList(
         character: Character,
         moveList: List<Move>,
     ): EmptyResult<WikiError> {
-        TODO("Not yet implemented")
+        return cacheMoveListUseCase.invoke(character, moveList)
+            .onError { Napier.e(tag = TAG) { "cacheMoveList: $it" } }
     }
 
     override suspend fun fetchMoveList(charName: String): Result<List<Move>, WikiError> {
-        TODO("Not yet implemented")
+        return fetchMoveListUseCase.invoke(charName)
+            .onError { Napier.e(tag = TAG) { "fetchMoveList: $it" } }
     }
 
     override suspend fun fetchMove(
         charName: String,
         moveQuery: String,
     ): Result<Move, WikiError> {
-        TODO("Not yet implemented")
+        return fetchMoveUseCase.invoke(charName, moveQuery)
+            .onError { Napier.w(tag = TAG) { "fetchMoveList: $it" } }
     }
 
     override suspend fun getLastUpdateTimeStamp(): Result<Instant?, WikiError> {
-        TODO("Not yet implemented")
+        return getLastCacheInsertInstantUseCase.invoke()
+            .onError { Napier.e(tag = TAG) { "getLastUpdateTimeStamp: $it" } }
     }
 
     override suspend fun clearCache(): EmptyResult<WikiError> {
-        TODO("Not yet implemented")
+        downloadOrFetchUseCase.clearCache()
+        return clearCacheUseCase.invoke()
+            .onError { Napier.e(tag = TAG) { "clearCache: $it" } }
+    }
+
+
+    private companion object {
+        const val TAG = "MizuumiWikiClient"
     }
 }
