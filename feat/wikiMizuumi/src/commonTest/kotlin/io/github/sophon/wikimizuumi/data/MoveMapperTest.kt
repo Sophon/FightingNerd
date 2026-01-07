3b6d0322c66af643766a3248be0c303428725d1b
Le Duy Anh <lenduya@gmail.com>
2026-01-07 12:48:20 +0100

    test (mizuumi): multiple url's in one property

--- feat/wikiMizuumi/src/commonTest/kotlin/io/github/sophon/wikimizuumi/data/MoveMapperTest.kt
+++ feat/wikiMizuumi/src/commonTest/kotlin/io/github/sophon/wikimizuumi/data/MoveMapperTest.kt
@@ -17,4 +17,20 @@ class MoveMapperTest {
         //then
         assertThat(result).isEqualTo(expected)
     }
+
+    @Test
+    fun `formPropertuesUrl creates markdown from multiple url`() {
+        // given
+        val string = "[[Melty Blood/MBTL/Glossary#Launch (L)|L]], [[Melty Blood/MBTL/Glossary#Ground Bounce (GB)|GB]]\n" +
+                "(second hit)"
+        val expected = "[L](https://mizuumi.wiki/w/Melty_Blood/MBTL/Glossary#Launch_(L)), " +
+                "[GB](https://mizuumi.wiki/w/Melty_Blood/MBTL/Glossary#Ground_Bounce_(GB))\n" +
+                "(second hit)"
+
+        // when
+        val result = string.formPropertiesUrl()
+
+        //then
+        assertThat(result).isEqualTo(expected)
+    }
 }
