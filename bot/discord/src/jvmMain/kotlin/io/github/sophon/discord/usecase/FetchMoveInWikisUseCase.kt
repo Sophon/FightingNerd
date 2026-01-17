843d1fc4ea2b421abe814001d78a4003486b3f6a
Le Duy Anh <lenduya@gmail.com>
2026-01-17 01:08:44 +0100

    feat (discord): fetch move in wikis usecase

---
+++ bot/discord/src/jvmMain/kotlin/io/github/sophon/discord/usecase/FetchMoveInWikisUseCase.kt
@@ -0,0 +1,24 @@
+package io.github.sophon.discord.usecase
+
+import io.github.sophon.core.domain.Result
+import io.github.sophon.core.wiki.domain.WikiClient
+import io.github.sophon.discord.BotError
+import io.github.sophon.discord.domain.BotOutput
+
+internal class FetchMoveInWikisUseCase {
+    suspend fun invoke(
+        wikis: Map<String, WikiClient>,
+        query: String,
+        searchFun: suspend (String, WikiClient, String) -> Result<BotOutput, BotError>,
+    ): Result<BotOutput, BotError> {
+        var lastError: BotError? = null
+        for ((gameId, wiki) in wikis) {
+            when (val result = searchFun(gameId, wiki, query)) {
+                is Result.Success -> return result
+                is Result.Error -> lastError = result.error
+            }
+        }
+
+        return Result.Error(lastError ?: BotError.UnknownMove(query))
+    }
+}
