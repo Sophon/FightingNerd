433af0aadc576a55c783a698b46b647a2dfbec4d
Le Duy Anh <lenduya@gmail.com>
2026-01-18 21:09:13 +0100

    refactor (discord): shorten the error descriptions and add buttons

--- bot/discord/src/jvmMain/kotlin/io/github/sophon/discord/usecase/CreateErrorEmbedUseCase.kt
+++ bot/discord/src/jvmMain/kotlin/io/github/sophon/discord/usecase/CreateErrorEmbedUseCase.kt
@@ -17,11 +17,17 @@ internal class CreateErrorEmbedUseCase {
             is BotError.UnknownCharacter,
             is BotError.UnknownMove,
                 -> {
-                syntaxErrorEmbed(error) to examplesButton()
+                syntaxErrorEmbed(error) to BotOutput.ButtonSet(
+                    buttonList = listOf(examplesButton(), commandsButton()),
+                    duration = EMBED_BUTTON_DURATION_INF.seconds,
+                )
             }
 
             is BotError.InvalidQuery -> {
-                createCommandSyntaxErrorEmbed(error) to commandsButton()
+                createCommandSyntaxErrorEmbed(error) to BotOutput.ButtonSet(
+                    buttonList = listOf(commandsButton(), helpButton()),
+                    duration = EMBED_BUTTON_DURATION_INF.seconds,
+                )
             }
 
             else -> {
@@ -44,17 +50,15 @@ internal class CreateErrorEmbedUseCase {
 
         description = unknownCharacterError
             .toString()
+            .let { "**$it**" }
             .truncate(EMBED_MAX_LENGTH)
 
         mandatoryField(
             name = "",
-            value = "THE **CHARACTER** NAME AND MOVE **INPUT** MUST HAVE **NO SPACES**. MOVE **NAME** CAN BE MULTI-WORD BUT MUST MATCH EXACTLY.\n\n" +
+            value = "The **character name** and move **input** must have __**NO SPACES**__.\n" +
+                    "**Move name** can be multi-word, but must match exactly.\n\n" +
                     "1. **`@bot`** → `@FightingNerdBot` `[character name]` `[move]`\n" +
-                    "2. **`/fd`** → `fd character: [character name] move: [move]`\n\n" +
-                    "- multi-word character name →`/alias` to see the available aliases\n" +
-                    "- stances or heat are followed by a dot: `nina h.b1+2` or `ak bad.3h.2`\n" +
-                    "- check `/heat`, `/homing`, `/stance`, `/pc` etc\n" +
-                    "- if stuck, check the wiki to see how it's written"
+                    "2. **`/fd`** → `fd character: [character name] move: [move]`\n"
         )
 
         footer {
@@ -71,6 +75,7 @@ internal class CreateErrorEmbedUseCase {
 
         description = invalidQueryError
             .toString()
+            .let { "**$it**" }
             .truncate(EMBED_MAX_LENGTH)
 
         mandatoryField(
@@ -87,39 +92,24 @@ internal class CreateErrorEmbedUseCase {
         }
     }
 
-    private fun examplesButton(): BotOutput.ButtonSet {
-        return BotOutput.ButtonSet(
-            buttonList = listOf(
-                BotOutput.EmbedButton(
-                    label = "EXAMPLES",
-                    action = BotOutput.EmbedButton.Action.Query("examples")
-                )
-            ),
-            duration = EMBED_BUTTON_DURATION_INF.seconds,
+    private fun examplesButton(): BotOutput.EmbedButton {
+        return BotOutput.EmbedButton(
+            label = "EXAMPLES",
+            action = BotOutput.EmbedButton.Action.Query("examples")
         )
     }
 
-    private fun commandsButton(): BotOutput.ButtonSet {
-        return BotOutput.ButtonSet(
-            buttonList = listOf(
-                BotOutput.EmbedButton(
-                    label = "COMMANDS",
-                    action = BotOutput.EmbedButton.Action.Query("commands")
-                )
-            ),
-            duration = EMBED_BUTTON_DURATION_INF.seconds,
+    private fun commandsButton(): BotOutput.EmbedButton {
+        return BotOutput.EmbedButton(
+            label = "COMMANDS",
+            action = BotOutput.EmbedButton.Action.Query("commands")
         )
     }
 
-    private fun helpButton(): BotOutput.ButtonSet {
-        return BotOutput.ButtonSet(
-            buttonList = listOf(
-                BotOutput.EmbedButton(
-                    label = "HELP",
-                    action = BotOutput.EmbedButton.Action.Query("help")
-                )
-            ),
-            duration = EMBED_BUTTON_DURATION_INF.seconds,
+    private fun helpButton(): BotOutput.EmbedButton {
+        return BotOutput.EmbedButton(
+            label = "HELP",
+            action = BotOutput.EmbedButton.Action.Query("help")
         )
     }
 }
