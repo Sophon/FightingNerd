e8274f32644d1984ec7541fc40dd9885e39ac51b
Le Duy Anh <lenduya@gmail.com>
2026-01-18 20:55:54 +0100

    feat (core): add game property to the DB interface

--- core/src/commonMain/kotlin/io/github/sophon/core/wiki/data/MoveListDB.kt
+++ core/src/commonMain/kotlin/io/github/sophon/core/wiki/data/MoveListDB.kt
@@ -2,6 +2,7 @@ package io.github.sophon.core.wiki.data
 
 import io.github.sophon.core.domain.EmptyResult
 import io.github.sophon.core.domain.Result
+import io.github.sophon.core.feature.Game
 import io.github.sophon.core.wiki.domain.model.Character
 import io.github.sophon.core.wiki.domain.model.Move
 import kotlinx.datetime.Instant
@@ -11,7 +12,7 @@ import kotlin.time.ExperimentalTime
 interface MoveListDB {
     suspend fun fetchMoveListFor(charName: String): Result<List<Move>, WikiError>
     suspend fun fetchMoveDataFor(charName: String, moveQuery: String): Result<Move, WikiError>
-    suspend fun insertMoveList(character: Character, moveList: List<Move>): EmptyResult<WikiError>
+    suspend fun insertMoveList(game: Game?, character: Character, moveList: List<Move>): EmptyResult<WikiError>
     suspend fun wipe(): EmptyResult<WikiError>
     suspend fun getLastInsertTimeStamp(): Result<Instant?, WikiError>
 }
