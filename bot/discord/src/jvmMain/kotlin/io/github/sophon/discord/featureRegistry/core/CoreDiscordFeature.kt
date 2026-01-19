1e1c9695428c1eebfae98dc1feb33aa8de3f3a95
Le Duy Anh <lenduya@gmail.com>
2026-01-19 05:22:56 +0100

    v8.4.1

--- bot/discord/src/jvmMain/kotlin/io/github/sophon/discord/featureRegistry/core/CoreDiscordFeature.kt
+++ bot/discord/src/jvmMain/kotlin/io/github/sophon/discord/featureRegistry/core/CoreDiscordFeature.kt
@@ -365,7 +365,7 @@ internal class CoreDiscordFeature(
     private companion object {
         const val TAG = "CoreDiscordFeature"
         const val PURPLE = 0x00A020F0
-        const val KEY_JOIN = "Steam lobby URL:"
-        const val KEY_PW = "Lobby password: "
+        const val KEY_JOIN = "steam lobby URL:"
+        const val KEY_PW = "lobby password: "
     }
 }
