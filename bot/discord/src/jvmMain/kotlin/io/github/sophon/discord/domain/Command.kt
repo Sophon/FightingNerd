71410c3cfb479cde424a91c0c8145cafd7065f7d
Le Duy Anh <lenduya@gmail.com>
2026-01-21 19:30:18 +0100

    refactor (discord): make Commands sealed class instead of enum

--- bot/discord/src/jvmMain/kotlin/io/github/sophon/discord/domain/Command.kt
+++ bot/discord/src/jvmMain/kotlin/io/github/sophon/discord/domain/Command.kt
@@ -1,84 +1,89 @@
 package io.github.sophon.discord.domain
 
 /**
- * Add registered service commands HERE
+ * Add registered feature commands HERE
  */
-enum class Command {
-    //core
-    TIP,
-    DONATE,
-    REPO,
-    INVITE,
-    HELP,
-    COMMANDS,
-    EXAMPLES,
-    JOIN,
 
-    //admin
-    FEEDBACK,
-    REPLY,
-    BAN,
-    UNBAN,
-    BANLIST,
+internal sealed class Command(val description: String) {
+    object Tip : Command(description = "Dono arigato!")
+    object Donate : Command(description = "Dono arigato!")
+    object Repo : Command(description = "Project repository")
+    object Invite : Command(description = "Bot invite link")
+    object Help : Command(description = "RTFM")
+    object Commands : Command(description = "Available commands")
+    object Examples : Command(description = "Command examples")
+    object Join : Command(description = "Make clickable Steam lobby link")
 
-    //general
-    FD,
+    // Admin commands
+    object Reply : Command(description = "Answer feedback")
+    object Ban : Command(description = "Ban user from using the bot")
+    object Unban : Command(description = "Unban user from using the bot")
+    object Banlist : Command(description = "List of users banned from using the bot")
 
-    //infil glossary
-    GL,
+    object Fd : Command(description = "Global frame data")
 
-    //wavu wiki
-    FDTK,
-    PC,
-    HEAT,
-    HOMING,
-    STANCE,
-    ALIASTK,
-    THROWTK,
+    object Gl: Command(description = "Fighting-game glossary")
 
-    //supercombo wiki
-    FDSF,
-    CHARSF,
-    FDMK,
-    CHARMK,
+    // Wavu
+    object FdTK : Command(description = "Tekken frame data")
+    object Pc : Command(description = "Tekken POWER CRUSH moves")
+    object Heat : Command(description = "Tekken HEAT moves")
+    object Homing : Command(description = "Tekken HOMING moves")
+    object Stance : Command(description = "Tekken STANCE moves")
+    object AliasTK : Command(description = "Tekken character aliases")
+    object ThrowTK : Command(description = "Tekken THROW moves")
 
-    //2xko wiki
-    FDXKO,
+    // SuperCombo
+    object FdSF : Command(description = "SF6 frame data")
+    object CharSF : Command(description = "SF6 character data")
+    object CharMK : Command(description = "MK1 character data")
+    object FdMK : Command(description = "MK1 frame data")
 
-    //dreamcancel wiki
-    FDKOF,
-    FDCOTW,
-    ALIASKOF,
-    ALIASCOTW,
+    // 2xko
+    object FdXko : Command(description = "2XKO frame data")
 
-    //dustloop wiki
-    CHARGG,
-    FDGG,
-    INVGG,
-    ALIASGG,
+    // DreamCancel
+    object FdKOF : Command(description = "KOF frame data")
+    object AliasKOF : Command(description = "KOF character aliases")
+    object FdCOTW : Command(description = "COTW frame data")
+    object AliasCOTW : Command(description = "COTW character aliases")
 
-    CHARDB,
-    FDDB,
+    // DustLoop commands
+    object CharGG : Command(description = "GG character data")
+    object FdGG : Command(description = "GG frame data")
+    object InvGG : Command(description = "GG invincible moves")
+    object AliasGG : Command(description = "GG character aliases")
 
-    CHARGB,
-    FDGB,
-    ALIASDB,
+    object CharDB : Command(description = "DB character data")
+    object FdDB : Command(description = "DB frame data")
+    object AliasDB : Command(description = "DB character aliases")
 
-    ALIASBB,
-    CHARBB,
-    FDBB,
-    INVBB,
+    object CharBB : Command(description = "BB character data")
+    object FdBB : Command(description = "BB frame data")
+    object AliasBB : Command(description = "BB character aliases")
+    object InvBB : Command(description = "BB invincible moves")
 
-    //mizuumi
-    ALIASMB,
-    FDMB,
-    INVMB,
+    object CharGB : Command(description = "GB character data")
+    object FdGB : Command(description = "GB frame data")
 
-    CHARUNI,
-    FDUNI,
-    INVUNI,
+    // Mizuumi
+    object FdMB : Command(description = "MBTL frame data")
+    object AliasMB : Command(description = "MBTL character aliases")
+    object InvMB : Command(description = "MBTL Invincible moves")
 
-    ALIASVS,
-    FDVS,
-    INVVS,
+    object FdUNI : Command(description = "Uni2 frame data")
+    object CharUNI : Command(description = "Uni2 character data")
+    object InvUNI : Command(description = "UNI2 Invincible moves")
+
+    object FdVS : Command(description = "VSAV frame data")
+    object InvVS : Command(description = "VSAV Invincible moves")
+    object AliasVS : Command(description = "VSAV character aliases")
+
+
+    companion object {
+        val entries: List<Command> by lazy {
+            Command::class.sealedSubclasses
+                .mapNotNull { it.objectInstance }
+        }
+    }
 }
