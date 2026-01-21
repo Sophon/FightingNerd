b54c1b0c38f089d89c014a7728daf8b2d449cbfe
Le Duy Anh <lenduya@gmail.com>
2026-01-21 16:54:56 +0100

    refactor (dustloop): extract toClickable to util

---
+++ feat/wikiDustLoop/src/commonMain/kotlin/io/github/sophon/wikidustloop/util/StringUtils.kt
@@ -0,0 +1,20 @@
+package io.github.sophon.wikidustloop.util
+
+import io.github.sophon.wikidustloop.WIKI_BASE_URL
+
+fun String?.toClickable(): String? {
+    if (this == null) return null
+
+    val regex = """\[\[.*?\]\]""".toRegex() // "text text [[match]] text [[match]] text.
+
+    return regex.replace(this) { matchResult ->
+        val content = matchResult.groupValues[1]
+        val fields = content.split("|")
+        val title = fields.lastOrNull() ?: ""
+        val partialUrl = fields.firstOrNull()
+            .orEmpty()
+            .replace(" ", "_")
+            .trim()
+        "[$title](${WIKI_BASE_URL}/$partialUrl)"
+    }
+}
--- feat/wikiDustLoop/src/commonTest/kotlin/io/github/sophon/wikidustloop/data/CharacterMapperTest.kt
+++ feat/wikiDustLoop/src/commonTest/kotlin/io/github/sophon/wikidustloop/data/CharacterMapperTest.kt
@@ -3,6 +3,7 @@ package io.github.sophon.wikidustloop.data
 import assertk.assertThat
 import assertk.assertions.isEqualTo
 import io.github.sophon.core.feature.Game
+import io.github.sophon.wikidustloop.util.toClickable
 import kotlin.test.Test
 
 class CharacterMapperTest {
