6e7ae2ccc29ab44481e551d59295e8b988897996
Le Duy Anh <lenduya@gmail.com>
2026-01-27 13:15:24 +0100

    refactor (discord): column logic for movelist

--- bot/discord/src/jvmMain/kotlin/io/github/sophon/discord/featureRegistry/core/Embeds.kt
+++ bot/discord/src/jvmMain/kotlin/io/github/sophon/discord/featureRegistry/core/Embeds.kt
@@ -3,6 +3,7 @@ package io.github.sophon.discord.featureRegistry.core
 import dev.kord.common.Color
 import dev.kord.rest.builder.message.EmbedBuilder
 import io.github.sophon.core.feature.FeatureInfo
+import io.github.sophon.discord.EMBED_LIST_MIN_COLUMN
 import io.github.sophon.discord.EMBED_LIST_PER_COLUMN
 import io.github.sophon.discord.util.featureFooter
 import io.github.sophon.discord.util.mandatoryField
@@ -15,14 +16,19 @@ internal fun moveListEmbed(
 ): EmbedBuilder.() -> Unit = {
     this.color = color
 
-    //don't join, chunk, then join
     val numberedMoves = dataList
         .mapIndexed { index, data ->
             "${index + 1}. **${data}**"
         }
 
+    val chunks = when (numberedMoves.size) {
+        in 0..EMBED_LIST_MIN_COLUMN -> numberedMoves.size
+        in EMBED_LIST_MIN_COLUMN..EMBED_LIST_PER_COLUMN -> numberedMoves.size / 2
+        else -> EMBED_LIST_PER_COLUMN
+    }
+
     numberedMoves
-        .chunked(EMBED_LIST_PER_COLUMN)
+        .chunked(chunks)
         .forEachIndexed { index, moveList ->
             val text = moveList.joinToString("\n")
             val name = if (index == 0) "$category moves" else "_"
