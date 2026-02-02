18732a5078fa4dfec452e8188a88340a782daa8b
Le Duy Anh <lenduya@gmail.com>
2026-02-02 17:17:03 +0100

    test (core): forward variant util

--- core/src/commonTest/kotlin/io/github/sophon/core/util/MoveInputUtilsTest.kt
+++ core/src/commonTest/kotlin/io/github/sophon/core/util/MoveInputUtilsTest.kt
@@ -10,23 +10,7 @@ class MoveInputUtilsTest {
     fun `aliases from slash handles 2x`() {
         // given
         val string = "j.5s1/j.2s1"
-        val expected = listOf(
-            "j.5s1",
-            "j.2s1",
-        )
-
-        // when
-        val result = string.createAliasesFromSlash(isPartial = false)
-
-        //then
-        assertThat(result).isEqualTo(expected)
-    }
-
-    @Test
-    fun `aliases from slash handles mizuumi`() {
-        // given
-        val string = "j4/6ad"
-        val expected = listOf("j4", "6ad")
+        val expected = listOf("j.5s1", "j.2s1")
 
         // when
         val result = string.createAliasesFromSlash(isPartial = false)
@@ -43,6 +27,60 @@ class MoveInputUtilsTest {
 
         // when
         val result = string.createAliasesFromSlash(isPartial = false)
+
+        //then
+        assertThat(result).isEqualTo(expected)
+    }
+    //endregion
+
+    //region
+    @Test
+    fun `use forward variant handles jump`() {
+        // given
+        val string = "j4/6ad"
+        val expected = "j6ad"
+
+        // when
+        val result = string.useForwardVariantOnly()
+
+        //then
+        assertThat(result).isEqualTo(expected)
+    }
+
+    @Test
+    fun `use forward variant handles normal slash`() {
+        // given
+        val string = "4/6a"
+        val expected = "6a"
+
+        // when
+        val result = string.useForwardVariantOnly()
+
+        //then
+        assertThat(result).isEqualTo(expected)
+    }
+
+    @Test
+    fun `use forward variant handles text close`() {
+        // given
+        val string = "c.4/6d"
+        val expected = "c.6d"
+
+        // when
+        val result = string.useForwardVariantOnly()
+
+        //then
+        assertThat(result).isEqualTo(expected)
+    }
+
+    @Test
+    fun `use forward variant handles keeps non back forward`() {
+        // given
+        val string = "214h/236d"
+        val expected = "214h/236d"
+
+        // when
+        val result = string.useForwardVariantOnly()
 
         //then
         assertThat(result).isEqualTo(expected)
