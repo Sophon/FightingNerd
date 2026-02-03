9e3720ba927b25bbff784bf604e270470b9d0855
Le Duy Anh <lenduya@gmail.com>
2026-02-03 20:49:42 +0100

    feat (ewgf): DI

--- feat/ewgf/src/commonMain/kotlin/io/github/sophon/ewgfModule.kt
+++ feat/ewgf/src/commonMain/kotlin/io/github/sophon/ewgfModule.kt
@@ -1,12 +1,19 @@
 package io.github.sophon
 
+import io.github.sophon.data.PlayerRepo
+import io.github.sophon.data.PlayerRepoImpl
+import io.github.sophon.domain.EwgfFeatureInfo
 import org.koin.core.module.Module
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 fun ewgfModule() = module {
     includes(platformModule)
 
-    //TODO
+    single { EwgfFeatureInfo }
+
+    singleOf(::PlayerRepoImpl).bind<PlayerRepo>()
 }
 
 expect val platformModule: Module
