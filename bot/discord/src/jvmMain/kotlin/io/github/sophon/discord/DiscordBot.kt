885c85ea4bb0e3dc98ce9174cbc08738af4fcffc
Le Duy Anh <lenduya@gmail.com>
2026-02-21 23:49:49 +0100

    feat (discord): test feedback redirect server

--- bot/discord/src/jvmMain/kotlin/io/github/sophon/discord/DiscordBot.kt
+++ bot/discord/src/jvmMain/kotlin/io/github/sophon/discord/DiscordBot.kt
@@ -77,9 +77,9 @@ internal class DiscordBotImpl(
     }
 
     private suspend fun startKord() {
-//        cleanOldGuildCommands(kord)
-//        createGlobalCommands()
-//        createAdminCommands()
+        cleanOldGuildCommands(kord)
+        createGlobalCommands()
+        createAdminCommands()
 //        createCommandsForTestServer()
 
         monitorGatewayHealth()
