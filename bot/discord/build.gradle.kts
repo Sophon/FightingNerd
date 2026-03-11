b224a1e1c6b698000f220b9ddea175a9a5830b7a
Le Duy Anh <lenduya@gmail.com>
2026-03-12 00:56:30 +0100

    chore (discord): version bump v10.1.0

--- bot/discord/build.gradle.kts
+++ bot/discord/build.gradle.kts
@@ -79,7 +79,7 @@ tasks.matching { it.name == "jvmRun" }.configureEach {
     (this as JavaExec).workingDir = rootProject.projectDir
 }
 
-val featureVersion = "9.1.1"
+val featureVersion = "10.1.0"
 buildkonfig {
     packageName = "io.github.sophon.discord"
 
