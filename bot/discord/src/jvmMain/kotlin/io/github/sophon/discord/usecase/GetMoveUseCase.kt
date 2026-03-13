d5c24daa339955a2306f0ab69afbb50e8ad0e143
Le Duy Anh <lenduya@gmail.com>
2026-03-13 02:03:40 +0100

    refactor (discord): manually remove spaces from the move query

--- bot/discord/src/jvmMain/kotlin/io/github/sophon/discord/usecase/GetMoveUseCase.kt
+++ bot/discord/src/jvmMain/kotlin/io/github/sophon/discord/usecase/GetMoveUseCase.kt
@@ -2,11 +2,11 @@ package io.github.sophon.discord.usecase
 
 import io.github.sophon.core.domain.Result
 import io.github.sophon.core.domain.mapError
+import io.github.sophon.core.util.dropFirstAndJoin
 import io.github.sophon.core.wiki.domain.WikiClient
 import io.github.sophon.core.wiki.domain.model.Move
 import io.github.sophon.discord.BotError
 import io.github.sophon.discord.domain.toDomainError
-import io.github.sophon.discord.util.parseQuery
 
 internal class GetMoveUseCase() {
     suspend fun invoke(
@@ -14,11 +14,25 @@ internal class GetMoveUseCase() {
         query: String
     ): Result<Move, BotError> {
         val parsedQuery = query.parseQuery()
-        if (parsedQuery == null) return Result.Error(BotError.UnknownMove(query))
+            ?: return Result.Error(BotError.UnknownMove(query))
 
         return wiki.fetchMove(
-            charName = parsedQuery.charName, moveQuery = parsedQuery.move
-        )
-            .mapError { it.toDomainError() }
+            charName = parsedQuery.charName,
+            moveQuery = parsedQuery.move.replace(" ", "")
+        ).mapError { it.toDomainError() }
+    }
+
+    internal data class ParsedQuery(
+        val charName: String,
+        val move: String,
+    )
+
+    internal fun String.parseQuery(): ParsedQuery? {
+        if (split(" ").size < 2) return null
+
+        val charName = substringBefore(' ')
+        val move = dropFirstAndJoin(' ')
+
+        return ParsedQuery(charName, move)
     }
 }
