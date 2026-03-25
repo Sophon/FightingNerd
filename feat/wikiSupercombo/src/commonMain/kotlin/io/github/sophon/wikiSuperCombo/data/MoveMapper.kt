47c6c09a964eef754b72045384986eed817bd19a
Le Duy Anh <lenduya@gmail.com>
2026-03-25 22:06:09 +0100

    refactor (supercombo): create Move DTO extension fun for better testability

--- feat/wikiSupercombo/src/commonMain/kotlin/io/github/sophon/wikiSuperCombo/data/MoveMapper.kt
+++ feat/wikiSupercombo/src/commonMain/kotlin/io/github/sophon/wikiSuperCombo/data/MoveMapper.kt
@@ -14,89 +14,96 @@ internal fun MoveListResponseDto.toDomain(
 ): List<Move> {
     return cargoQuery.map { wrapper ->
         val dto = wrapper.title
-
-        val type = dto.getType()
-        val move = Move(
-            charName = dto.chara,
-            id = dto.moveId,
-            name = dto.name,
-
-            input = dto.input.cleanMoveInput(),
-            damage = dto.damage.takeIfNotTemplate()?.cleanHtml(),
-            startup = dto.startup.takeIfNotTemplate(),
-            onBlock = dto.blockAdv.takeIfNotTemplate()?.cleanHtml(),
-            onHit = dto.hitAdv.takeIfNotTemplate()?.cleanHtml(),  
-            onCH = null,
-            recovery = dto.recovery.takeIfNotTemplate()?.cleanHtml(),
-            notes = dto.notes.takeIfNotTemplate()?.cleanHtml()
-                .extractNotes(),
-            active = dto.active.takeIfNotTemplate()?.cleanHtml(),
-            guard = dto.guard.takeIfNotTemplate(),
-            cancel = dto.cancel.takeIfNotTemplate(),
-            invulnerability = dto.invuln.takeIfNotTemplate()?.cleanHtml(),
-
-            aliases = dto.formAliases(type),
-
-            urls = Move.Urls(
-                characterImage = characterData.imageUrl,
-                moveImageList = dto.images
-                    .orEmpty()
-                    .split(",")
-                    .map { it.trim() }
-                    .mapNotNull { imageUrlMap.getOrElse(key = it, defaultValue = { null }) },
-                hitboxImageList = dto.hitboxes
-                    .orEmpty()
-                    .split(",")
-                    .map { it.trim() }
-                    .mapNotNull { imageUrlMap.getOrElse(key = it, defaultValue = { null }) },
-                wikiUrl = formMoveWikiUrl(
-                    gameId = gameId,
-                    charName = dto.chara,
-                    input = dto.input,
-                    name = dto.name,
-                )
-            ),
-
-            sf6Properties = Move.SF6Properties(
-                type = type,
-                images = dto.images.takeIfNotTemplate()
-                    ?.split(",")
-                    ?.map { it.trim() },
-                chip = dto.chip.takeIfNotTemplate(),
-                dmgScaling = dto.dmgScaling.takeIfNotTemplate(),
-                total = dto.total.takeIfNotTemplate(),
-                hitConfirm = dto.hitconfirm.takeIfNotTemplate(),
-                punishAdv = dto.punishAdv.takeIfNotTemplate()?.cleanHtml(),  
-                perfParryAdv = dto.perfParryAdv.takeIfNotTemplate()?.cleanHtml(),  
-                DRcOH = dto.DRcancelHit.takeIfNotTemplate()?.cleanHtml(),  
-                DRcOB = dto.DRcancelBlk.takeIfNotTemplate()?.cleanHtml(),  
-                DROH = dto.afterDRHit.takeIfNotTemplate()?.cleanHtml(),  
-                DROB = dto.afterDRBlk.takeIfNotTemplate()?.cleanHtml(),
-                hitStun = dto.hitstun.takeIfNotTemplate()?.cleanHtml(),  
-                blockStun = dto.blockstun.takeIfNotTemplate()?.cleanHtml(),  
-                hitStop = dto.hitstop.takeIfNotTemplate()?.cleanHtml(),  
-                driveDmgOnBlock = dto.driveDmgBlk.takeIfNotTemplate(),
-                driveDmgOnHit = dto.driveDmgHit.takeIfNotTemplate(),
-                driveGain = dto.driveGain.takeIfNotTemplate(),
-                superGainOnHit = dto.superGainHit.takeIfNotTemplate(),
-                superGainOnBlock = dto.superGainBlk.takeIfNotTemplate(),
-                armor = dto.armor.takeIfNotTemplate(),
-                jugStart = dto.jugStart.takeIfNotTemplate()?.cleanHtml(),  
-                jugIncrease = dto.jugIncrease.takeIfNotTemplate()?.cleanHtml(),  
-                jugLimit = dto.jugLimit.takeIfNotTemplate(),
-                projectileSpeed = dto.projSpeed.takeIfNotTemplate(),
-                attackRange = dto.atkRange.takeIfNotTemplate(),
-            ),
-            mkProperties = Move.MKProperties(
-                moveType = dto.moveType.takeIfNotTemplate(),
-                cost = dto.moveType
-                    .split(",")
-                    .filterNot { it.takeIfNotTemplate() == null }
-            )
-        )
-
+        val move = dto.toDomain(gameId, characterData, imageUrlMap)
         move
     }
+}
+
+fun MoveDto.toDomain(
+    gameId: String,
+    characterData: DownloadMoveListUseCase.CharacterData,
+    imageUrlMap: Map<String, String>,
+): Move {
+    val type = getType()
+
+    return Move(
+        charName = chara,
+        id = moveId,
+        name = name,
+
+        input = input.cleanMoveInput(),
+        damage = damage.takeIfNotTemplate()?.cleanHtml(),
+        startup = startup.takeIfNotTemplate(),
+        onBlock = blockAdv.takeIfNotTemplate()?.cleanHtml(),
+        onHit = hitAdv.takeIfNotTemplate()?.cleanHtml(),
+        onCH = null,
+        recovery = recovery.takeIfNotTemplate()?.cleanHtml(),
+        notes = notes.takeIfNotTemplate()?.cleanHtml()
+            .extractNotes(),
+        active = active.takeIfNotTemplate()?.cleanHtml(),
+        guard = guard.takeIfNotTemplate(),
+        cancel = cancel.takeIfNotTemplate(),
+        invulnerability = invuln.takeIfNotTemplate()?.cleanHtml(),
+
+        aliases = formAliases(type),
+
+        urls = Move.Urls(
+            characterImage = characterData.imageUrl,
+            moveImageList = images
+                .orEmpty()
+                .split(",")
+                .map { it.trim() }
+                .mapNotNull { imageUrlMap.getOrElse(key = it, defaultValue = { null }) },
+            hitboxImageList = hitboxes
+                .orEmpty()
+                .split(",")
+                .map { it.trim() }
+                .mapNotNull { imageUrlMap.getOrElse(key = it, defaultValue = { null }) },
+            wikiUrl = formMoveWikiUrl(
+                gameId = gameId,
+                charName = chara,
+                input = input,
+                name = name,
+            )
+        ),
+
+        sf6Properties = Move.SF6Properties(
+            type = type,
+            images = images.takeIfNotTemplate()
+                ?.split(",")
+                ?.map { it.trim() },
+            chip = chip.takeIfNotTemplate(),
+            dmgScaling = dmgScaling.takeIfNotTemplate(),
+            total = total.takeIfNotTemplate(),
+            hitConfirm = hitconfirm.takeIfNotTemplate(),
+            punishAdv = punishAdv.takeIfNotTemplate()?.cleanHtml(),
+            perfParryAdv = perfParryAdv.takeIfNotTemplate()?.cleanHtml(),
+            DRcOH = DRcancelHit.takeIfNotTemplate()?.cleanHtml(),
+            DRcOB = DRcancelBlk.takeIfNotTemplate()?.cleanHtml(),
+            DROH = afterDRHit.takeIfNotTemplate()?.cleanHtml(),
+            DROB = afterDRBlk.takeIfNotTemplate()?.cleanHtml(),
+            hitStun = hitstun.takeIfNotTemplate()?.cleanHtml(),
+            blockStun = blockstun.takeIfNotTemplate()?.cleanHtml(),
+            hitStop = hitstop.takeIfNotTemplate()?.cleanHtml(),
+            driveDmgOnBlock = driveDmgBlk.takeIfNotTemplate(),
+            driveDmgOnHit = driveDmgHit.takeIfNotTemplate(),
+            driveGain = driveGain.takeIfNotTemplate(),
+            superGainOnHit = superGainHit.takeIfNotTemplate(),
+            superGainOnBlock = superGainBlk.takeIfNotTemplate(),
+            armor = armor.takeIfNotTemplate(),
+            jugStart = jugStart.takeIfNotTemplate()?.cleanHtml(),
+            jugIncrease = jugIncrease.takeIfNotTemplate()?.cleanHtml(),
+            jugLimit = jugLimit.takeIfNotTemplate(),
+            projectileSpeed = projSpeed.takeIfNotTemplate(),
+            attackRange = atkRange.takeIfNotTemplate(),
+        ),
+        mkProperties = Move.MKProperties(
+            moveType = moveType.takeIfNotTemplate(),
+            cost = moveType
+                .split(",")
+                .filterNot { it.takeIfNotTemplate() == null }
+        )
+    )
 }
 
 private fun String?.takeIfNotTemplate(): String? {
@@ -154,7 +161,7 @@ private fun MoveDto.formAliases(type: Move.SF6Properties.Type?): List<String> {
         else -> this.input.formMotionInput()
     }
 
-    return alias?.let { listOf(it) } ?: listOf()
+    return alias?.let { listOf(it.lowercase()) } ?: listOf()
 }
 
 private fun formSuperLevel(
@@ -176,8 +183,8 @@ private fun String?.formMotionInput(): String? {
         this == null -> return null
         this.startsWith("41236") -> this.replaceFirst("41236", "hcf")
         this.startsWith("63214") -> this.replaceFirst("63214", "hcb")
-        this.startsWith("214") -> this.replaceFirst("214", "qcb")
-        this.startsWith("236") -> this.replaceFirst("236", "qcf")
+        this.startsWith("214") -> this.replace("214", "qcb")
+        this.startsWith("236") -> this.replace("236", "qcf")
         this.startsWith("623") -> this.replaceFirst("623", "dp")
         this.startsWith("421") -> this.replaceFirst("421", "bdp")
         this.startsWith("360") -> this.replaceFirst("360", "spd")
