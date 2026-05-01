9e12b1a94a9c3139508e3e5a9d14f05f4f75fe77
Le Duy Anh <lenduya@gmail.com>
2026-05-02 01:51:07 +0200

    fix (bottomBar): tweaks

--- composeApp/src/commonMain/kotlin/io/github/sophon/fightingnerd/feat/bottomBar/ui/BottomBarView.kt
+++ composeApp/src/commonMain/kotlin/io/github/sophon/fightingnerd/feat/bottomBar/ui/BottomBarView.kt
@@ -2,13 +2,14 @@ package io.github.sophon.fightingnerd.feat.bottomBar.ui
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -17,6 +18,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -54,19 +56,21 @@ private fun BottomBarContent(
             .fillMaxWidth()
             .background(color = Color.Transparent),
     ) {
-        LazyRow(
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(2.dp),
             modifier = Modifier
                 .background(
                     color = MaterialTheme.colorScheme.surfaceContainerHigh.copy(alpha = 0.9f),
                     shape = RoundedCornerShape(48),
                 )
-                .padding(vertical = 8.dp, horizontal = 24.dp)
+                .padding(8.dp)
         ) {
-            itemsIndexed(state.itemList) { index, item ->
+            state.itemList.forEachIndexed { index, item ->
                 BarItem(
                     item = item,
                     isSelected = (index == state.selectedItemIndex),
                     onClick = { onItemClick(index) },
+                    modifier = Modifier.widthIn(min = 96.dp)
                 )
             }
         }
@@ -94,34 +98,30 @@ private fun BarItem(
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier
-            .clickable(enabled = true, onClick = onClick),
+            .clip(RoundedCornerShape(48))
+            .clickable(enabled = true, onClick = onClick)
+            .background(
+                color = backgroundColor,
+                shape = RoundedCornerShape(48),
+            )
+            .padding(8.dp),
     ) {
-        Box(
-            modifier = Modifier
-                .background(
-                    color = backgroundColor,
-                    shape = RoundedCornerShape(50),
+        when (val icon = item.icon) {
+            is FlexibleIcon.Vector -> {
+                Icon(
+                    imageVector = icon.imageVector,
+                    contentDescription = null,
+                    modifier = Modifier.size(32.dp),
+                    tint = itemColor,
                 )
-                .padding(horizontal = 16.dp, vertical = 4.dp),
-            contentAlignment = Alignment.Center,
-        ) {
-            when (val icon = item.icon) {
-                is FlexibleIcon.Vector -> {
-                    Icon(
-                        imageVector = icon.imageVector,
-                        contentDescription = null,
-                        modifier = Modifier.size(32.dp),
-                        tint = itemColor,
-                    )
-                }
-                is FlexibleIcon.Resource -> {
-                    Icon(
-                        painter = painterResource(icon.drawableResource),
-                        contentDescription = null,
-                        modifier = Modifier.size(16.dp),
-                        tint = itemColor,
-                    )
-                }
+            }
+            is FlexibleIcon.Resource -> {
+                Icon(
+                    painter = painterResource(icon.drawableResource),
+                    contentDescription = null,
+                    modifier = Modifier.size(32.dp),
+                    tint = itemColor,
+                )
             }
         }
 
