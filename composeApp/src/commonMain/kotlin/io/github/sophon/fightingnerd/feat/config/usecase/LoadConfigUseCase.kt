b1feec05f8a34b68c8c3110f95630897152a541b
Le Duy Anh <lenduya@gmail.com>
2026-05-20 17:51:27 +0200

    refactor (module): rename variable in config json

--- composeApp/src/commonMain/kotlin/io/github/sophon/fightingnerd/feat/config/usecase/LoadConfigUseCase.kt
+++ composeApp/src/commonMain/kotlin/io/github/sophon/fightingnerd/feat/config/usecase/LoadConfigUseCase.kt
@@ -4,7 +4,9 @@ import fightingnerd.composeapp.generated.resources.Res
 import io.github.aakira.napier.Napier
 import io.github.sophon.core.domain.Result
 import io.github.sophon.core.feature.Config
+import io.github.sophon.core.util.getGame
 import io.github.sophon.fightingnerd.core.model.AppError
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.SerializationException
 import kotlinx.serialization.json.Json
 import org.jetbrains.compose.resources.MissingResourceException
@@ -15,9 +17,21 @@ internal class LoadConfigUseCase(
     suspend fun invoke(): Result<Config, AppError> {
         val result = try {
             val moduleListString = Res.readBytes(CONFIG_PATH).decodeToString()
-            val config = json.decodeFromString<Config>(moduleListString).apply {
+            val jsonConfig = json.decodeFromString<JsonConfig>(moduleListString).apply {
                 Napier.d(tag = TAG) { this.toString() }
             }
+            val config = Config(
+                featureList = jsonConfig.featureList.map { feature ->
+                    Config.Feature(
+                        name = feature.name,
+                        isEnabled = feature.isEnabled,
+                        supportedGameList = feature.supportedGameList.mapNotNull { gameId ->
+                            gameId.getGame()
+                        }
+                    )
+                }
+            )
+
             Result.Success(config)
         } catch (e: MissingResourceException) {
             val errorMessage = e.message ?: "Config file not found"
@@ -30,6 +44,19 @@ internal class LoadConfigUseCase(
         }
 
         return result
+    }
+
+    @Serializable
+    private data class JsonConfig(
+        val featureList: List<Feature>,
+    ) {
+        @Serializable
+        data class Feature(
+            val name: String,
+            val isEnabled: Boolean,
+            val supportedGameList: List<String>,
+            val feedbackDiscordChannelId: String? = null,
+        )
     }
 
 
