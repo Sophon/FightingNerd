19b56c64fc8ed1de1147585c4a62e7939e03ec3f
Le Duy Anh <lenduya@gmail.com>
2026-05-21 19:45:29 +0200

    feat (module): bind the rest of wiki modules

--- composeApp/src/commonMain/kotlin/io/github/sophon/fightingnerd/Module.kt
+++ composeApp/src/commonMain/kotlin/io/github/sophon/fightingnerd/Module.kt
@@ -1,11 +1,15 @@
 package io.github.sophon.fightingnerd
 
 import io.github.sophon.core.coreModule
+import io.github.sophon.dreamcancel.integration.dreamCancelModule
 import io.github.sophon.fightingnerd.feat.featureModule
 import io.github.sophon.fightingnerd.featureRegistry.featureRegistryModule
 import io.github.sophon.fightingnerd.screens.screensModule
 import io.github.sophon.wikiSuperCombo.integration.superComboModule
+import io.github.sophon.wikidustloop.integration.dustLoopModule
+import io.github.sophon.wikimizuumi.integration.mizuumiModule
 import io.github.sophon.wikiwavu.integration.wavuModule
+import io.github.sophon.xko.integration.xkoModule
 import org.koin.core.context.startKoin
 import org.koin.core.module.Module
 import org.koin.dsl.KoinAppDeclaration
@@ -19,6 +23,10 @@ internal fun initKoin(config: KoinAppDeclaration? = null) = startKoin {
         coreModule,
         wavuModule(),
         superComboModule(),
+        xkoModule(),
+        dreamCancelModule(),
+        dustLoopModule(),
+        mizuumiModule(),
 
         screensModule,
 
