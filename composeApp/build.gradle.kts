9fe7597bd384f8f2002ea90c06fd21e765798b64
Le Duy Anh <lenduya@gmail.com>
2026-05-25 01:46:52 +0200

    chore (app): navigation 3 vm lifecycle

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -62,6 +62,7 @@ kotlin {
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
             implementation(libs.jetbrains.navigation3.ui)
+            implementation(libs.jetbrains.lifecycle.viewmodelNavigation3)
 
             api(libs.koin.core)
             implementation(libs.koin.compose)
