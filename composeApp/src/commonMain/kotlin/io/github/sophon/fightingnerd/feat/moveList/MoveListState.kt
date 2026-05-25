62e80ef2449cccac6d6eefef72b36fbf7b0bfacd
Le Duy Anh <lenduya@gmail.com>
2026-05-25 16:54:30 +0200

    feat (moveList): state

--- composeApp/src/commonMain/kotlin/io/github/sophon/fightingnerd/feat/moveList/MoveListState.kt
+++ composeApp/src/commonMain/kotlin/io/github/sophon/fightingnerd/feat/moveList/MoveListState.kt
@@ -3,9 +3,29 @@ package io.github.sophon.fightingnerd.feat.moveList
 import io.github.sophon.core.feature.Game
 import io.github.sophon.core.wiki.domain.model.Character
 import io.github.sophon.core.wiki.domain.model.Move
+import io.github.sophon.fightingnerd.feat.moveList.model.Property
 
 internal data class MoveListState(
     val character: Character?,
     val game: Game?,
-    val moveList: List<Move> = emptyList(),
-)
+    val moveList: Map<String, Move> = emptyMap(),
+
+    val uiMoveList: List<UiMove> = emptyList(),
+    val moveDetail: MoveDetail? = null,
+) {
+    data class UiMove(
+        val id: String,
+
+        val startup: String,
+        val level: String,
+        val propertySet: Set<Property> = emptySet(),
+
+        val onHit: String,
+        val onBlock: String,
+        val onCounter: String,
+    )
+
+    data class MoveDetail(
+        val move: Move,
+    )
+}
---
+++ composeApp/src/commonMain/kotlin/io/github/sophon/fightingnerd/feat/moveList/model/Property.kt
@@ -0,0 +1,10 @@
+package io.github.sophon.fightingnerd.feat.moveList.model
+
+internal enum class Property {
+    Invincible,
+    PowerCrush,
+    Homing,
+    HighCrush,
+    LowCrush,
+    Heat,
+}
