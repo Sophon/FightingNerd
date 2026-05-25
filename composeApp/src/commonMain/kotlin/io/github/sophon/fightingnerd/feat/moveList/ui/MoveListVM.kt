d482544a64af1401fa8d4a7b072957b209a5b70f
Le Duy Anh <lenduya@gmail.com>
2026-05-25 19:05:47 +0200

    feat (moveList): map move list

--- composeApp/src/commonMain/kotlin/io/github/sophon/fightingnerd/feat/moveList/ui/MoveListVM.kt
+++ composeApp/src/commonMain/kotlin/io/github/sophon/fightingnerd/feat/moveList/ui/MoveListVM.kt
@@ -6,6 +6,8 @@ import io.github.aakira.napier.Napier
 import io.github.sophon.core.domain.onError
 import io.github.sophon.core.domain.onSuccess
 import io.github.sophon.core.feature.Game
+import io.github.sophon.core.wiki.domain.model.Move
+import io.github.sophon.fightingnerd.feat.moveList.model.Property
 import io.github.sophon.fightingnerd.feat.moveList.usecase.LoadMoveListUseCase
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
@@ -48,13 +50,42 @@ internal class MoveListVM(
                 game = game,
                 characterId = characterId,
             )
-                .onSuccess {
-                    Napier.d(tag = TAG) { "Moves loaded: ${it.size}" }
+                .onSuccess { moveList ->
+                    Napier.d(tag = TAG) { "Moves loaded: ${moveList.size}" }
+                    val uiMoveList = moveList.map { it.toUiMove() }
+                    _state.update { state ->
+                        state.copy(
+                            fullMoveList = moveList.associateBy { it.id },
+                            uiMoveList = uiMoveList,
+                        )
+                    }
                 }
                 .onError {
                     //TODO: error event
                 }
         }
+    }
+
+    private fun Move.toUiMove(): MoveListState.UiMove {
+        return MoveListState.UiMove(
+            id = id,
+            input = input,
+            startup = startup,
+            level = guard,
+            onHit = onHit,
+            onBlock = onBlock,
+            onCounter = onCH,
+            propertySet = buildSet {
+                invulnerability?.let { add(Property.Invincible) }
+                t8Properties?.let { props ->
+                    if (props.isHeat) add(Property.Heat)
+                    if (props.isHoming) add(Property.Homing)
+                    if (props.isPowerCrush) add(Property.PowerCrush)
+                    if (props.isHighCrush) add(Property.HighCrush)
+                    if (props.isLowCrush) add(Property.LowCrush)
+                }
+            },
+        )
     }
 
 
