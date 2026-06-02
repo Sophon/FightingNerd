8f96061b4188e34a9cdfb8ce2fc5df38d4506eea
Le Duy Anh <lenduya@gmail.com>
2026-06-02 20:02:59 +0200

    refactor (home): rework the usecase to check for cached character list first

--- composeApp/src/commonMain/kotlin/io/github/sophon/fightingnerd/feat/home/usecase/LoadGameCharacterListUseCase.kt
+++ composeApp/src/commonMain/kotlin/io/github/sophon/fightingnerd/feat/home/usecase/LoadGameCharacterListUseCase.kt
@@ -1,8 +1,10 @@
 package io.github.sophon.fightingnerd.feat.home.usecase
 
 import io.github.sophon.core.domain.Result
-import io.github.sophon.core.domain.map
+import io.github.sophon.core.domain.flatMap
 import io.github.sophon.core.domain.mapError
+import io.github.sophon.core.wiki.domain.WikiClient
+import io.github.sophon.core.wiki.domain.model.Character
 import io.github.sophon.fightingnerd.core.model.AppError
 import io.github.sophon.fightingnerd.feat.home.ui.HomeViewState
 import io.github.sophon.fightingnerd.feat.module.ModuleRepo
@@ -16,26 +18,59 @@ internal class LoadGameCharacterListUseCase(
         val wikiClient = moduleRepo.getWikiClientFor(gameWidget.game)
             ?: return Result.Error(AppError.WikiClientNotFound(gameWidget.game.id))
 
-        val result = wikiClient.downloadCharacterList()
-            .map { characterList ->
-                val loadedWidget = gameWidget.copy(
-                    characterList = characterList.map { domainCharacter ->
-                        HomeViewState.GameWidget.Character(
-                            id = domainCharacter.id,
-                            displayName = domainCharacter.displayName,
-                            queryName = domainCharacter.queryName,
-                            iconUrl = domainCharacter.images?.iconUrl,
-                        )
-                    },
-                    isLoading = false,
-                )
-                loadedWidget
+        val result = getCachedCharacterList(wikiClient).flatMap { cachedCharacterList ->
+            if (cachedCharacterList.isEmpty()) {
+                downloadCharacterList(wikiClient).flatMap { downloadedCharacterList ->
+                    wikiClient.cacheCharacterList(downloadedCharacterList)
+                        .flatMap {
+                            val loadedWidget = gameWidget.updateWithMoveList(downloadedCharacterList)
+                            Result.Success(loadedWidget)
+                        }
+                        .mapError { wikiError ->
+                            val appError = AppError.WikiError(wikiError.toString())
+                            appError
+                        }
+                }
+            } else {
+                val loadedWidget = gameWidget.updateWithMoveList(cachedCharacterList)
+                Result.Success(loadedWidget)
             }
+        }
+
+        return result
+    }
+
+
+    private suspend fun getCachedCharacterList(wikiClient: WikiClient): Result<List<Character>, AppError> {
+        val result = wikiClient.fetchCharacterList()
             .mapError { wikiError ->
                 val appError = AppError.WikiError(wikiError.toString())
                 appError
             }
-
         return result
+    }
+
+    private suspend fun downloadCharacterList(wikiClient: WikiClient): Result<List<Character>, AppError> {
+        val result = wikiClient.downloadCharacterList()
+            .mapError { wikiError ->
+                val appError = AppError.WikiError(wikiError.toString())
+                appError
+            }
+        return result
+    }
+
+    private fun HomeViewState.GameWidget.updateWithMoveList(moveList: List<Character>): HomeViewState.GameWidget {
+        val updatedWidget = this.copy(
+            characterList = moveList.map { domainCharacter ->
+                HomeViewState.GameWidget.Character(
+                    id = domainCharacter.id,
+                    displayName = domainCharacter.displayName,
+                    queryName = domainCharacter.queryName,
+                    iconUrl = domainCharacter.images?.iconUrl,
+                )
+            },
+            isLoading = false,
+        )
+        return updatedWidget
     }
 }
