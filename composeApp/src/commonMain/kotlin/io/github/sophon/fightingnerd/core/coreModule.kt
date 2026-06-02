4275154ac98221595217105e869a2d8abd71b595
Le Duy Anh <lenduya@gmail.com>
2026-06-03 01:13:32 +0200

    feat (sql): DI for move DB

--- composeApp/src/commonMain/kotlin/io/github/sophon/fightingnerd/core/coreModule.kt
+++ composeApp/src/commonMain/kotlin/io/github/sophon/fightingnerd/core/coreModule.kt
@@ -5,25 +5,28 @@ import io.github.sophon.core.wiki.data.CharacterListDB
 import io.github.sophon.core.wiki.data.MoveListDB
 import io.github.sophon.fightingnerd.core.data.DatabaseDriverFactory
 import io.github.sophon.fightingnerd.core.data.SqlCharacterDB
-import io.github.sophon.fightingnerd.core.data.TmpMemoryMoveListDB
+import io.github.sophon.fightingnerd.core.data.SqlMoveDB
 import io.github.sophon.fightingnerd.db.character.CharacterDatabase
+import io.github.sophon.fightingnerd.db.move.MoveDatabase
 import org.koin.core.module.dsl.singleOf
-import org.koin.dsl.bind
 import org.koin.dsl.module
 
 internal fun coreModule() = module {
     singleOf(::MoveRepository)
-    singleOf(::TmpMemoryMoveListDB).bind<MoveListDB>()
 
     single<(Game) -> Pair<CharacterListDB, MoveListDB>> {
         val driverFactory: DatabaseDriverFactory = get()
-        val moveListDB: MoveListDB = get()
 
         return@single { game ->
             val charDbName = "${game.wiki.id}_${game.id}_characters.db"
             val charDriver = driverFactory.create(charDbName)
             val charDb = CharacterDatabase(charDriver)
             val characterListDB: CharacterListDB = SqlCharacterDB(charDb, game.id)
+
+            val moveDbName = "${game.wiki.id}_${game.id}_moves.db"
+            val moveDriver = driverFactory.create(moveDbName)
+            val moveDb = MoveDatabase(moveDriver)
+            val moveListDB: MoveListDB = SqlMoveDB(moveDb, game.id)
 
             characterListDB to moveListDB
         }
