d112f44140f61c5536cca115a4979cbd5552a11b
Le Duy Anh <lenduya@gmail.com>
2026-06-03 11:13:59 +0200

    refactor (sql): DI for move database

--- composeApp/src/commonMain/kotlin/io/github/sophon/fightingnerd/core/coreModule.kt
+++ composeApp/src/commonMain/kotlin/io/github/sophon/fightingnerd/core/coreModule.kt
@@ -19,12 +19,12 @@ internal fun coreModule() = module {
 
         return@single { game ->
             val charDbName = "${game.wiki.id}_${game.id}_characters.db"
-            val charDriver = driverFactory.create(charDbName)
+            val charDriver = driverFactory.create(databaseName = charDbName, schema = CharacterDatabase.Schema)
             val charDb = CharacterDatabase(charDriver)
             val characterListDB: CharacterListDB = SqlCharacterDB(charDb, game.id)
 
             val moveDbName = "${game.wiki.id}_${game.id}_moves.db"
-            val moveDriver = driverFactory.create(moveDbName)
+            val moveDriver = driverFactory.create(databaseName = moveDbName, schema = MoveDatabase.Schema)
             val moveDb = MoveDatabase(moveDriver)
             val moveListDB: MoveListDB = SqlMoveDB(moveDb, game.id)
 
