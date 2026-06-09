4496d02bbf3e4074374675fd81eae2af583b745d
Le Duy Anh <lenduya@gmail.com>
2026-06-09 22:04:19 +0200

    feat (navigation): add destinations

--- composeApp/src/commonMain/kotlin/io/github/sophon/fightingnerd/navigation/Destination.kt
+++ composeApp/src/commonMain/kotlin/io/github/sophon/fightingnerd/navigation/Destination.kt
@@ -1,6 +1,7 @@
 package io.github.sophon.fightingnerd.navigation
 
 import androidx.navigation3.runtime.NavKey
+import io.github.sophon.core.wiki.model.Move
 import kotlinx.serialization.Serializable
 
 sealed interface Destination: NavKey {
@@ -8,8 +9,20 @@ sealed interface Destination: NavKey {
     data object Home: Destination
 
     @Serializable
-    data class MoveList(
-        val gameId: String,
-        val characterId: String,
-    ): Destination
+    data object Search: Destination
+
+    @Serializable
+    data object Saved: Destination
+
+    @Serializable
+    data object Quiz: Destination
+
+    @Serializable
+    data class MoveList(val gameId: String, val characterId: String): Destination
+
+    @Serializable
+    data class MoveDetail(val move: Move): Destination
+
+    @Serializable
+    data class CharacterDetail(val gameId: String, val characterId: String): Destination
 }
