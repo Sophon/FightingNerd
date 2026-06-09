7f7e1c685d44e29111f54fe8ac233e31e093af91
Le Duy Anh <lenduya@gmail.com>
2026-06-10 00:22:15 +0200

    fix (navigation): top status bar padding

--- composeApp/src/commonMain/kotlin/io/github/sophon/fightingnerd/App.kt
+++ composeApp/src/commonMain/kotlin/io/github/sophon/fightingnerd/App.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
@@ -82,7 +83,9 @@ internal fun App() {
                         rememberSaveableStateHolderNavEntryDecorator(),
                         rememberViewModelStoreNavEntryDecorator(),
                     ),
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .statusBarsPadding(),
                     entryProvider = entryProvider {
                         entry<Destination.Home> {
                             HomeScreen(
