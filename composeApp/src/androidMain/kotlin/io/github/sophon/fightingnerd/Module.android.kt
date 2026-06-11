2d850509f11826cbd5db2223c94b7c2df6708c7f
Le Duy Anh <lenduya@gmail.com>
2026-06-11 12:58:02 +0200

    feat (compose): core single select dialog

--- composeApp/src/androidMain/kotlin/io/github/sophon/fightingnerd/Module.android.kt
+++ composeApp/src/androidMain/kotlin/io/github/sophon/fightingnerd/Module.android.kt
@@ -1,11 +1,17 @@
 package io.github.sophon.fightingnerd
 
 import io.github.sophon.fightingnerd.core.data.DatabaseDriverFactory
+import io.github.sophon.fightingnerd.core.domain.UrlOpener
+import io.github.sophon.fightingnerd.core.domain.UrlOpenerAnd
 import io.github.sophon.fightingnerd.infrastructure.createDataStore
 import org.koin.android.ext.koin.androidContext
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 internal actual val platformModule = module {
     single { createDataStore() }
     single { DatabaseDriverFactory(androidContext()) }
+
+    singleOf(::UrlOpenerAnd).bind<UrlOpener>()
 }
--- composeApp/src/iosMain/kotlin/io/github/sophon/fightingnerd/Module.ios.kt
+++ composeApp/src/iosMain/kotlin/io/github/sophon/fightingnerd/Module.ios.kt
@@ -1,10 +1,15 @@
 package io.github.sophon.fightingnerd
 
 import io.github.sophon.fightingnerd.core.data.DatabaseDriverFactory
+import io.github.sophon.fightingnerd.core.domain.UrlOpener
+import io.github.sophon.fightingnerd.core.domain.UrlOpenerIos
 import io.github.sophon.fightingnerd.infrastructure.createDataStore
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 internal actual val platformModule = module {
     single { createDataStore() }
     single { DatabaseDriverFactory() }
+    singleOf(::UrlOpenerIos).bind<UrlOpener>()
 }
