e8ec6caeed1b5eb2510626fef423a6459d829389
Le Duy Anh <lenduya@gmail.com>
2026-06-24 03:44:57 +0200

    performance (http): use Dispatchers IO for safecall

--- core/src/commonMain/kotlin/io/github/sophon/core/network/HttpClientExt.kt
+++ core/src/commonMain/kotlin/io/github/sophon/core/network/HttpClientExt.kt
@@ -6,24 +6,28 @@ import io.ktor.client.call.body
 import io.ktor.client.network.sockets.SocketTimeoutException
 import io.ktor.client.statement.HttpResponse
 import io.ktor.util.network.UnresolvedAddressException
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
 import kotlinx.coroutines.ensureActive
-import kotlin.coroutines.coroutineContext
+import kotlinx.coroutines.withContext
 
 suspend inline fun <reified T> safeCall(
-    request: () -> HttpResponse,
+    crossinline request: suspend () -> HttpResponse,
 ): Result<T, DataError.Remote> {
-    val response = try {
-        request()
-    } catch (e: SocketTimeoutException) {
-        return Result.Error(DataError.Remote.REQUEST_TIMEOUT)
-    } catch (e: UnresolvedAddressException) {
-        return Result.Error(DataError.Remote.NO_INTERNET)
-    } catch (e: Exception) {
-        coroutineContext.ensureActive()
-        return Result.Error(DataError.Remote.UNKNOWN)
-    }
+    return withContext(Dispatchers.IO) {
+        val response = try {
+            request()
+        } catch (e: SocketTimeoutException) {
+            return@withContext Result.Error(DataError.Remote.REQUEST_TIMEOUT)
+        } catch (e: UnresolvedAddressException) {
+            return@withContext Result.Error(DataError.Remote.NO_INTERNET)
+        } catch (e: Exception) {
+            coroutineContext.ensureActive()
+            return@withContext Result.Error(DataError.Remote.UNKNOWN)
+        }
 
-    return response.toResult()
+        return@withContext response.toResult()
+    }
 }
 
 suspend inline fun <reified T> HttpResponse.toResult(): Result<T, DataError.Remote> {
