bae1694a242cda73fae3624dca4738b83c8a89e1
Le Duy Anh <lenduya@gmail.com>
2026-06-25 22:44:36 +0200

    refactor (core): cleanup

--- core/src/commonMain/kotlin/io/github/sophon/core/wiki/data/DataErrorMapper.kt
+++ core/src/commonMain/kotlin/io/github/sophon/core/wiki/data/DataErrorMapper.kt
@@ -2,8 +2,6 @@ package io.github.sophon.core.wiki.data
 
 import io.github.sophon.core.architecture.DataError
 
-fun DataError.Remote.toDomainError(
-    tag: String,
-): WikiError {
+fun DataError.Remote.toDomainError(): WikiError {
     return WikiError.DownloadError(this.toString())
 }
