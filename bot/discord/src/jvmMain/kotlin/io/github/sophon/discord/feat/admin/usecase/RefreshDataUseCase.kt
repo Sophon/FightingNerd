f0f2c9fb61519da03b39ced758569884ef13da08
Le Duy Anh <lenduya@gmail.com>
2026-06-28 16:08:36 +0200

    v12.1.5

--- bot/discord/src/jvmMain/kotlin/io/github/sophon/discord/feat/admin/usecase/RefreshDataUseCase.kt
+++ bot/discord/src/jvmMain/kotlin/io/github/sophon/discord/feat/admin/usecase/RefreshDataUseCase.kt
@@ -2,14 +2,14 @@ package io.github.sophon.discord.feat.admin.usecase
 
 import io.github.sophon.core.architecture.EmptyResult
 import io.github.sophon.core.architecture.Result
+import io.github.sophon.discord.feat.config.BotFeatureRepo
 import io.github.sophon.discord.feat.core.domain.model.BotError
-import io.github.sophon.discord.feat.core.domain.model.DiscordRegisteredFeature
 
 internal class RefreshDataUseCase(
-    private val featureList: Lazy<List<DiscordRegisteredFeature>>,
+    private val featureRepo: Lazy<BotFeatureRepo>,
 ) {
     suspend fun invoke(): EmptyResult<BotError> {
-        featureList.value.forEach { feature ->
+        featureRepo.value.getFeatures().forEach { feature ->
             feature.refreshData()
         }
 
