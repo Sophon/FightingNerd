abf9cd8548fdf924a77e8b559bd8ec705ecd6394
Le Duy Anh <lenduya@gmail.com>
2026-07-16 14:27:39 +0200

    refactor (move): remove unused data class

--- composeApp/src/commonMain/kotlin/io/github/sophon/fightingnerd/feat/move/ui/UiMove.kt
+++ composeApp/src/commonMain/kotlin/io/github/sophon/fightingnerd/feat/move/ui/UiMove.kt
@@ -17,15 +17,6 @@ internal data class UiMove(
         val value: String?,
     )
 
-    data class CoreFields(
-        val startup: Field,
-        val guard: Field,
-        val damage: Field,
-        val onBlock: Field,
-        val onHit: Field,
-        val onCH: Field,
-    )
-
     fun isExpandable(): Boolean {
         return move.notes.isNotEmpty() || move.urls.videoId.isNullOrEmpty().not()
     }
