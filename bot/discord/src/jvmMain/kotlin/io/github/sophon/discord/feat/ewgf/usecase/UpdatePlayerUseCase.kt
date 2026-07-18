836fe0e656bc8e0f1cda1623918fc9754353cc40
Le Duy Anh <lenduya@gmail.com>
2026-07-18 13:19:11 +0200

    test (ewgf): exclude client call usecases from test coverage

--- bot/discord/src/jvmMain/kotlin/io/github/sophon/discord/feat/ewgf/usecase/UpdatePlayerUseCase.kt
+++ bot/discord/src/jvmMain/kotlin/io/github/sophon/discord/feat/ewgf/usecase/UpdatePlayerUseCase.kt
@@ -2,11 +2,13 @@ package io.github.sophon.discord.feat.ewgf.usecase
 
 import io.github.sophon.integration.EwgfClient
 import io.github.sophon.core.architecture.EmptyResult
+import io.github.sophon.core.architecture.ExcludeFromCoverage
 import io.github.sophon.core.architecture.mapError
 import io.github.sophon.discord.feat.core.domain.model.BotError
 import io.github.sophon.discord.feat.core.domain.toDomainError
 import io.github.sophon.integration.model.Player
 
+@ExcludeFromCoverage("plain client call")
 internal class UpdatePlayerUseCase(
     private val client: EwgfClient,
 ) {
