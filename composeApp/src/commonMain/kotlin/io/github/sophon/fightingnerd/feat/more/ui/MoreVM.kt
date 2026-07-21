4718bcb2f5fa4602a5f063f969d51faf23465555
Le Duy Anh <lenduya@gmail.com>
2026-07-21 03:03:02 +0200

    refactor (more): remove redundant usecase

--- composeApp/src/commonMain/kotlin/io/github/sophon/fightingnerd/feat/more/ui/MoreVM.kt
+++ composeApp/src/commonMain/kotlin/io/github/sophon/fightingnerd/feat/more/ui/MoreVM.kt
@@ -5,7 +5,6 @@ import androidx.lifecycle.viewModelScope
 import io.github.sophon.fightingnerd.core.usecase.OpenUrlUseCase
 import io.github.sophon.fightingnerd.feat.more.model.DonationMethod
 import io.github.sophon.fightingnerd.feat.more.model.MoreItem
-import io.github.sophon.fightingnerd.feat.more.usecase.SubscribeToThemeUseCase
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -16,7 +15,6 @@ import kotlinx.coroutines.launch
 
 internal class MoreVM(
     private val openUrlUseCase: OpenUrlUseCase,
-    private val subscribeToThemeUseCase: SubscribeToThemeUseCase,
 ): ViewModel() {
     private val _state = MutableStateFlow(MoreState())
     val state = _state.asStateFlow()
@@ -28,7 +26,6 @@ internal class MoreVM(
     fun onItemClick(item: MoreItem) {
         viewModelScope.launch {
             when (item) {
-//                MoreItem.Theme -> onThemeDialog(isDialogVisible = true)
                 MoreItem.FeatureSettings -> _navEvent.send(item)
             }
         }
