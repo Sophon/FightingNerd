8b90abc371117362e87713788adb23e4b5ef486f
Le Duy Anh <lenduya@gmail.com>
2026-07-24 16:03:09 +0200

    fix (dragdown): properly formed character ID for multi word names

--- feat/wikiDragDown/src/commonMain/kotlin/io/github/sophon/wikidragdown/data/CharacterMapper.kt
+++ feat/wikiDragDown/src/commonMain/kotlin/io/github/sophon/wikidragdown/data/CharacterMapper.kt
@@ -9,60 +9,66 @@ internal fun List<CharacterResponseDto>.toDomain(
     imageUrlMap: Map<String, String>,
     gameId: String,
 ): List<Character> {
-    val characterList = this.map { dto ->
-        val id = dto.chara.lowercase()
-        val iconFilename = dto.chara.formIcon()
-
-        val character = Character(
-            id = id,
-            displayName = dto.chara,
-            remoteQueryId = dto.chara,
-            wikiUrl = dto.chara.formWikiUrl(gameId),
-            images = Character.Images(
-                iconUrl = imageUrlMap[iconFilename],
-            ),
-            roa2Properties = Character.Roa2Properties(
-                dacusSpeedMultiplier = dto.dacusSpeedMultiplier?.toString(),
-                weight = dto.weight?.toString(),
-                frictionGround = dto.frictionGround?.toString(),
-                frictionAir = dto.frictionAir?.toString(),
-                dashFrames = dto.dashFrames?.toString(),
-                dashSpeed = dto.dashSpeed?.toString(),
-                dashAcceleration = dto.dashAcceleration?.toString(),
-                runSpeedMax = dto.runSpeedMax?.toString(),
-                runTurnAcceleration = dto.runTurnAcceleration?.toString(),
-                runTurnFrames = dto.runTurnFrames?.toString(),
-                walkAccelerationMax = dto.walkAccelerationMax?.toString(),
-                walkSpeedMax = dto.walkSpeedMax?.toString(),
-                gravity = dto.gravity?.toString(),
-                hitstunGravity = dto.hitstunGravity?.toString(),
-                fallSpeedMax = dto.fallSpeedMax?.toString(),
-                fastFallSpeed = dto.fastFallSpeed?.toString(),
-                airAcceleration = dto.airAcceleration?.toString(),
-                airSpeedHorizontalMax = dto.airSpeedHorizontalMax?.toString(),
-                jumpSpeedHorizontalMax = dto.jumpSpeedHorizontalMax?.toString(),
-                fullHopSpeed = dto.fullHopSpeed?.toString(),
-                shortHopSpeed = dto.shortHopSpeed?.toString(),
-                doubleJumpSpeed = dto.doubleJumpSpeed?.toString(),
-                doubleJumpMaxHorizontalSpeed = dto.doubleJumpMaxHorizontalSpeed?.toString(),
-                airDodgeSpeed = dto.airDodgeSpeed?.toString(),
-                airDodgeFriction = dto.airDodgeFriction?.toString(),
-                rollSpeed = dto.rollSpeed?.toString(),
-                shieldSizeMultiplier = dto.shieldSizeMultiplier?.toString(),
-                ledgeStandSpeed = dto.ledgeStandSpeed?.toString(),
-                ledgeRollSpeed = dto.ledgeRollSpeed?.toString(),
-                ledgeJumpMaxHorizontalAirSpeed = dto.ledgeJumpMaxHorizontalAirSpeed?.toString(),
-                getupRollSpeed = dto.getupRollSpeed?.toString(),
-                techRollSpeed = dto.techRollSpeed?.toString(),
-                wallJumpSpeedY = dto.wallJumpSpeedY?.toString(),
-                wallJumpSpeedX = dto.wallJumpSpeedX?.toString(),
-            )
-        )
-
-        character
-    }
+    val characterList = this.map { it.toDomain(imageUrlMap, gameId) }
 
     return characterList
+}
+
+internal fun CharacterResponseDto.toDomain(
+    imageUrlMap: Map<String, String>,
+    gameId: String,
+): Character {
+    val dto = this
+    val id = dto.chara.formId()
+    val iconFilename = dto.chara.formIcon()
+
+    val character = Character(
+        id = id,
+        displayName = dto.chara,
+        remoteQueryId = dto.chara,
+        wikiUrl = dto.chara.formWikiUrl(gameId),
+        images = Character.Images(
+            iconUrl = imageUrlMap[iconFilename],
+        ),
+        roa2Properties = Character.Roa2Properties(
+            dacusSpeedMultiplier = dto.dacusSpeedMultiplier?.toString(),
+            weight = dto.weight?.toString(),
+            frictionGround = dto.frictionGround?.toString(),
+            frictionAir = dto.frictionAir?.toString(),
+            dashFrames = dto.dashFrames?.toString(),
+            dashSpeed = dto.dashSpeed?.toString(),
+            dashAcceleration = dto.dashAcceleration?.toString(),
+            runSpeedMax = dto.runSpeedMax?.toString(),
+            runTurnAcceleration = dto.runTurnAcceleration?.toString(),
+            runTurnFrames = dto.runTurnFrames?.toString(),
+            walkAccelerationMax = dto.walkAccelerationMax?.toString(),
+            walkSpeedMax = dto.walkSpeedMax?.toString(),
+            gravity = dto.gravity?.toString(),
+            hitstunGravity = dto.hitstunGravity?.toString(),
+            fallSpeedMax = dto.fallSpeedMax?.toString(),
+            fastFallSpeed = dto.fastFallSpeed?.toString(),
+            airAcceleration = dto.airAcceleration?.toString(),
+            airSpeedHorizontalMax = dto.airSpeedHorizontalMax?.toString(),
+            jumpSpeedHorizontalMax = dto.jumpSpeedHorizontalMax?.toString(),
+            fullHopSpeed = dto.fullHopSpeed?.toString(),
+            shortHopSpeed = dto.shortHopSpeed?.toString(),
+            doubleJumpSpeed = dto.doubleJumpSpeed?.toString(),
+            doubleJumpMaxHorizontalSpeed = dto.doubleJumpMaxHorizontalSpeed?.toString(),
+            airDodgeSpeed = dto.airDodgeSpeed?.toString(),
+            airDodgeFriction = dto.airDodgeFriction?.toString(),
+            rollSpeed = dto.rollSpeed?.toString(),
+            shieldSizeMultiplier = dto.shieldSizeMultiplier?.toString(),
+            ledgeStandSpeed = dto.ledgeStandSpeed?.toString(),
+            ledgeRollSpeed = dto.ledgeRollSpeed?.toString(),
+            ledgeJumpMaxHorizontalAirSpeed = dto.ledgeJumpMaxHorizontalAirSpeed?.toString(),
+            getupRollSpeed = dto.getupRollSpeed?.toString(),
+            techRollSpeed = dto.techRollSpeed?.toString(),
+            wallJumpSpeedY = dto.wallJumpSpeedY?.toString(),
+            wallJumpSpeedX = dto.wallJumpSpeedX?.toString(),
+        )
+    )
+
+    return character
 }
 
 private fun String.formWikiUrl(gameId: String): String {
@@ -70,4 +76,13 @@ private fun String.formWikiUrl(gameId: String): String {
     val game = Game.fromId(gameId)
     val url = "${game?.wikiUrl ?: BASE_URL}/$formatted"
     return url
+}
+
+private fun String.formId(): String {
+    val id = this
+        .lowercase()
+        .split(" ")
+        .takeLast(1)
+        .joinToString()
+    return id
 }
