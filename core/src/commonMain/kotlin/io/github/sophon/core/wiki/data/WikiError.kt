8233842471d2951774aa2723e276853a72db387d
Le Duy Anh <lenduya@gmail.com>
2026-08-10 01:02:44 +0200

    refactor (core): page not found error

--- core/src/commonMain/kotlin/io/github/sophon/core/wiki/data/WikiError.kt
+++ core/src/commonMain/kotlin/io/github/sophon/core/wiki/data/WikiError.kt
@@ -4,6 +4,7 @@ import io.github.sophon.core.architecture.Error
 
 sealed class WikiError(private val name: String, vararg val inputs: String) : Error {
     class DownloadError(input: String) : WikiError("DownloadError", input)
+    class PageNotFound(input: String) : WikiError("PageNotFound", input)
     class DatabaseError(input: String) : WikiError("DatabaseError", input)
     class UnknownCharacter(input: String) : WikiError("UnknownCharacter", input)
     class UnknownMove(vararg inputs: String) : WikiError("UnknownMove", *inputs)
