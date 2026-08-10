718a995f21ac7661d813d9a233396087186b35d2
Le Duy Anh <lenduya@gmail.com>
2026-08-10 19:15:45 +0200

    refactor (discord): replace the old client funs

--- bot/discord/src/jvmMain/kotlin/io/github/sophon/discord/feat/core/usecase/GetCharactersUseCase.kt
+++ bot/discord/src/jvmMain/kotlin/io/github/sophon/discord/feat/core/usecase/GetCharactersUseCase.kt
@@ -2,16 +2,15 @@ package io.github.sophon.discord.feat.core.usecase
 
 import io.github.sophon.core.architecture.ExcludeFromCoverage
 import io.github.sophon.core.architecture.Result
-import io.github.sophon.core.architecture.mapError
 import io.github.sophon.core.wiki.model.Character
 import io.github.sophon.core.wiki.model.WikiClient
 import io.github.sophon.discord.feat.core.domain.model.BotError
-import io.github.sophon.discord.feat.core.domain.toDomainError
+import kotlinx.coroutines.flow.first
 
 @ExcludeFromCoverage("plain client call")
 internal class GetCharactersUseCase {
     suspend fun invoke(wiki: WikiClient): Result<List<Character>, BotError> {
-        return wiki.fetchCharacterList()
-            .mapError { it.toDomainError() }
+        val characterList = wiki.subscribeToCharacterList().first()
+        return Result.Success(characterList)
     }
 }
