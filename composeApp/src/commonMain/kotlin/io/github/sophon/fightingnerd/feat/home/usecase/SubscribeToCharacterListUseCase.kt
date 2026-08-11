9c668893747e6d483c9133fbd5dd0c23fee5625a
Le Duy Anh <lenduya@gmail.com>
2026-08-11 14:25:28 +0200

    fix (home): pass character list while refreshing data

--- composeApp/src/commonMain/kotlin/io/github/sophon/fightingnerd/feat/home/usecase/SubscribeToCharacterListUseCase.kt
+++ composeApp/src/commonMain/kotlin/io/github/sophon/fightingnerd/feat/home/usecase/SubscribeToCharacterListUseCase.kt
@@ -4,8 +4,9 @@ import io.github.sophon.core.featureConfig.FeatureRepo
 import io.github.sophon.core.wiki.model.Character
 import io.github.sophon.fightingnerd.feat.home.ui.HomeViewState
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.channelFlow
 import kotlinx.coroutines.flow.emptyFlow
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 
 internal class SubscribeToCharacterListUseCase(
     private val featureRepo: FeatureRepo,
@@ -14,12 +15,16 @@ internal class SubscribeToCharacterListUseCase(
         val wikiClient = featureRepo.getWikiClientFor(gameWidget.game)
             ?: return emptyFlow()
 
-        val characterListFlow = wikiClient.subscribeToCharacterList()
-            .onEach { characters ->
-                if (characters.isEmpty()) {
-                    wikiClient.refreshData()
+        val characterListFlow = channelFlow {
+            var refreshTriggered = false
+            wikiClient.subscribeToCharacterList().collect { characters ->
+                send(characters)
+                if (characters.isEmpty() && refreshTriggered.not()) {
+                    refreshTriggered = true
+                    launch { wikiClient.refreshData() }
                 }
             }
+        }
         return characterListFlow
     }
 }
