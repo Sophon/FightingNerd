03737c82b79836a993ec3f6e72fbfc31d438ebda
Le Duy Anh <lenduya@gmail.com>
2026-08-15 16:22:00 +0200

    refactor (more): new tip button

--- composeApp/src/commonMain/kotlin/io/github/sophon/fightingnerd/feat/featureModule.kt
+++ composeApp/src/commonMain/kotlin/io/github/sophon/fightingnerd/feat/featureModule.kt
@@ -3,20 +3,23 @@ package io.github.sophon.fightingnerd.feat
 import io.github.sophon.fightingnerd.feat.home.ui.HomeVM
 import io.github.sophon.fightingnerd.feat.home.usecase.CheckCharacterHasMovesUseCase
 import io.github.sophon.fightingnerd.feat.home.usecase.CheckIfFirstLaunchUseCase
-import io.github.sophon.fightingnerd.feat.home.usecase.SubscribeToGamesUseCase
 import io.github.sophon.fightingnerd.feat.home.usecase.RefreshUseCase
 import io.github.sophon.fightingnerd.feat.home.usecase.SubscribeToCharacterListUseCase
+import io.github.sophon.fightingnerd.feat.home.usecase.SubscribeToGamesUseCase
 import io.github.sophon.fightingnerd.feat.module.domain.WikiClientFactory
 import io.github.sophon.fightingnerd.feat.module.usecase.LoadConfigUseCase
 import io.github.sophon.fightingnerd.feat.more.ui.MoreVM
-import io.github.sophon.fightingnerd.feat.move.ui.MoveListVM
 import io.github.sophon.fightingnerd.feat.more.ui.featureSettings.FeatureSettingsVM
 import io.github.sophon.fightingnerd.feat.more.usecase.GetAvailableFeaturesUseCase
 import io.github.sophon.fightingnerd.feat.more.usecase.SaveFeatureConfigUseCase
 import io.github.sophon.fightingnerd.feat.more.usecase.SubscribeToThemeUseCase
+import io.github.sophon.fightingnerd.feat.move.ui.MoveListVM
 import io.github.sophon.fightingnerd.feat.move.usecase.LoadMoveFiltersUseCase
 import io.github.sophon.fightingnerd.feat.move.usecase.NormalizeSliderUseCase
 import io.github.sophon.fightingnerd.feat.move.usecase.SubscribeToMoveListUseCase
+import io.github.sophon.fightingnerd.feat.payment.ui.TipVM
+import io.github.sophon.fightingnerd.feat.payment.usecase.GetTipOptionsUseCase
+import io.github.sophon.fightingnerd.feat.payment.usecase.PurchaseTipUseCase
 import io.github.sophon.fightingnerd.feat.quiz.ui.overview.QuizOverviewVM
 import io.github.sophon.fightingnerd.feat.quiz.ui.quiz.QuizVM
 import io.github.sophon.fightingnerd.feat.quiz.usecase.GenerateQuestionsUseCase
@@ -80,5 +83,11 @@ internal fun featureModule() = module {
 
     singleOf(::GenerateQuestionsUseCase)
     singleOf(::SubscribeGameWidgetsUseCase)
+    //endregion
+
+    //region Payment
+    singleOf(::GetTipOptionsUseCase)
+    singleOf(::PurchaseTipUseCase)
+    viewModelOf(::TipVM)
     //endregion
 }
