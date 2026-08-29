87773e4d33e34c576943ec4584da56c00370d362
Le Duy Anh <lenduya@gmail.com>
2026-08-29 23:52:22 +0200

    test (discord): fix command

--- bot/discord/src/jvmTest/kotlin/io/github/sophon/botdiscord/feat/bot/usecase/RouteCommandToFeatureUseCaseTest.kt
+++ bot/discord/src/jvmTest/kotlin/io/github/sophon/botdiscord/feat/bot/usecase/RouteCommandToFeatureUseCaseTest.kt
@@ -134,7 +134,7 @@ class RouteCommandToFeatureUseCaseTest {
             version = "1.0.0"
         )
         override val defaultCommand = Command.Fd
-        override val otherCommands = listOf(Command.CharSF)
+        override val otherCommands = listOf(Command.Char)
 
         override suspend fun start() {}
 
@@ -164,7 +164,7 @@ class RouteCommandToFeatureUseCaseTest {
                         Result.Success(BotOutput(primaryEmbedBuilder = { title = "SuperCombo FD: $query" }))
                     }
                 }
-                Command.CharSF -> {
+                Command.Char -> {
                     if (query.lowercase() in sfChars) {
                         Result.Success(BotOutput(primaryEmbedBuilder = { title = "SuperCombo CHARSF6: $query" }))
                     } else {
