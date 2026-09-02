8b0d98688dce7a7e0cf38a8c45ad37b4e350a641
Le Duy Anh <lenduya@gmail.com>
2026-09-02 15:41:39 +0200

    refactor (core): operator invoke

--- composeApp/src/commonMain/kotlin/io/github/sophon/fightingnerd/core/usecase/OpenUrlUseCase.kt
+++ composeApp/src/commonMain/kotlin/io/github/sophon/fightingnerd/core/usecase/OpenUrlUseCase.kt
@@ -7,7 +7,7 @@ import io.github.sophon.fightingnerd.core.domain.UrlOpener
 internal class OpenUrlUseCase(
     private val urlOpener: UrlOpener,
 ) {
-    fun invoke(url: String) {
+    operator fun invoke(url: String) {
         urlOpener.openUrl(url)
     }
 }
