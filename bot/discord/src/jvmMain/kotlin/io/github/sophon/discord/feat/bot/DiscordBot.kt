65687cb503fb7980b2928eafccf80e38a0d52bac
Le Duy Anh <lenduya@gmail.com>
2026-09-03 12:09:20 +0200

    refactor (discord): catch command create interaction event

--- bot/discord/src/jvmMain/kotlin/io/github/sophon/discord/feat/bot/DiscordBot.kt
+++ bot/discord/src/jvmMain/kotlin/io/github/sophon/discord/feat/bot/DiscordBot.kt
@@ -82,10 +82,12 @@ internal class DiscordBotImpl(
         monitorGatewayHealth()
 
         kord.on<GuildChatInputCommandInteractionCreateEvent> {
-            handleQueryUseCase.invoke(
-                interaction = interaction,
-                editableEmbedMap = editableEmbedMap,
-            )
+            kordRestCall(TAG) {
+                handleQueryUseCase.invoke(
+                    interaction = interaction,
+                    editableEmbedMap = editableEmbedMap,
+                )
+            }
         }
 
         kord.on<MessageCreateEvent> {
