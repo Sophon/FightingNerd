57f1b274b0d639951bba409d8131ed68f36802e6
Le Duy Anh <lenduya@gmail.com>
2026-09-03 12:48:58 +0200

    refactor (kord): pass Source to kord rest call

--- bot/discord/src/jvmMain/kotlin/io/github/sophon/discord/util/DiscordUtils.kt
+++ bot/discord/src/jvmMain/kotlin/io/github/sophon/discord/util/DiscordUtils.kt
@@ -3,17 +3,25 @@ package io.github.sophon.discord.util
 import dev.kord.core.exception.EntityNotFoundException
 import dev.kord.rest.request.KtorRequestException
 import io.github.aakira.napier.Napier
+import io.github.sophon.integration.model.Source
 
-internal suspend fun kordRestCall(tag: String, block: suspend () -> Unit) {
+internal suspend fun kordRestCall(
+    tag: String,
+    source: Source? = null,
+    block: suspend () -> Unit,
+) {
+    val prefix = source?.serverName
+        ?.takeIf { it.isNotBlank() }
+        ?: "(Unknown server)"
     try {
         block()
     } catch (e: KtorRequestException) {
         when (e.status.code) {
-            403 -> Napier.w(tag = tag) { "⚠️ Missing access (bot not in guild?)" }
-            404 -> Napier.w(tag = tag) { "⚠️ Resource already gone (deleted message?)" }
-            else -> Napier.e(tag = tag) { "💥 Unexpected REST error: ${e.message}" }
+            403 -> Napier.w(tag = tag) { "⚠️ ${prefix}: Missing access (bot not in guild?)" }
+            404 -> Napier.w(tag = tag) { "⚠️ ${prefix}: Resource already gone (deleted message?)" }
+            else -> Napier.e(tag = tag) { "💥 ${prefix}: Unexpected REST error: ${e.message}" }
         }
     } catch (_: EntityNotFoundException) {
-        Napier.w(tag = tag) { "️ Initial interaction response for interaction was not found" }
+        Napier.w(tag = tag) { "️ ${prefix}: Initial interaction response for interaction was not found" }
     }
 }
